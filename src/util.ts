0702af86cce14ce1a5ba906ec8e7c1cce0732cd5
guillermooo <guillermo.lopez@outlook.com>
2015-11-17 21:41:38 +0100

    assorted fixes

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,7 @@ import * as vscode from 'vscode';
 export function showInfo(message : string) : void {
     vscode.window.showInformationMessage("Vim: " + message);
 }
+
+export function showError(message : string) : void {
+    vscode.window.showErrorMessage("Vim: " + message);
+}
