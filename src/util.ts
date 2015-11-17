f2950a144c190ac1c8a55c4915830cdcbad16635
Jason Poon <git@jasonpoon.ca>
2015-11-16 22:24:47 -0800

    add gulp + tslint

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
 
 export function showInfo(message : string) : void {
-	vscode.window.showInformationMessage("Vim: " + message);
+    vscode.window.showInformationMessage("Vim: " + message);
 }
