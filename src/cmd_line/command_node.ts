a765d5a0617f65e94288eadd4ed25971cdd32b79
guillermooo <guillermo.lopez@outlook.com>
2015-11-19 22:26:32 +0100

    implement :w!

--- src/cmd_line/command_node.ts
+++ src/cmd_line/command_node.ts
@@ -1,28 +1,79 @@
-import * as vscode from 'vscode';
-import * as node from './node';
-import * as util from '../util';
+import fs = require('fs');
+
+import vscode = require('vscode');
+
+import node = require('./node');
+import util = require('../util');
+
+export interface WriteCommandArguments {
+	opt? : string;
+	optValue? : string;
+	bang? : boolean;
+	range? : node.LineRange;
+	file? : string;
+	append? : boolean;
+	cmd? : string;
+}
 
 //
 //  Implements :write
 //  http://vimdoc.sourceforge.net/htmldoc/editing.html#:write
 //
 export class WriteCommand implements node.CommandBase {
-    name : string;
-    shortName : string;
-    args : Object;
+	name : string;
+	shortName : string;
+	args : WriteCommandArguments;
 
-    constructor(args : Object = null) {
-        // TODO: implement other arguments.
-        this.name = 'write';
-        this.shortName = 'w';
-        this.args = args;
-    }
+	constructor(args : WriteCommandArguments = {}) {
+		this.name = 'write';
+		this.shortName = 'w';
+		this.args = args;
+	}
 
-    runOn(textEditor : vscode.TextEditor) : void {
-        if (this.args) {
-            util.showError("Not implemented.");
-            return;
-        }
-        textEditor.document.save();
-    }
+	private doSave(textEditor : vscode.TextEditor) {
+		textEditor.document.save().then(
+			(ok) => {
+				if (ok) {
+					util.showInfo("File saved.");
+				} else {
+					util.showError("File not saved.");
+				}
+			},
+			(e) => util.showError(e)
+		);
+	};
+
+	runOn(textEditor : vscode.TextEditor) : void {
+		if (this.args.opt) {
+			util.showError("Not implemented.");
+			return;
+		} else if (this.args.file) {
+			util.showError("Not implemented.");
+			return;
+		} else if (this.args.append) {
+			util.showError("Not implemented.");
+			return;
+		} else if (this.args.cmd) {
+			util.showError("Not implemented.");
+			return;
+		}
+
+		fs.access(textEditor.document.fileName, fs.W_OK, (accessErr) => {
+			if (accessErr) {
+				if (this.args.bang) {
+					fs.chmod(textEditor.document.fileName, 666, (e) => {
+						if (e) {
+							util.showError(e.message);
+						} else {
+							this.doSave(textEditor);
+						}
+					});
+				} else {
+					util.showError(accessErr.message);
+				}
+			} else {
+				this.doSave(textEditor);
+			}
+		});
+	}
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -6,36 +6,36 @@ import * as token from '../src/cmd_line/token';
 
 suite("Cmd line tests - parser", () => {
 
-    test("can parse empty string", () => {
-        var cmd = parser.parse("");
-        assert.ok(cmd.isEmpty);
-    });
+	test("can parse empty string", () => {
+		var cmd = parser.parse("");
+		assert.ok(cmd.isEmpty);
+	});
 
-    // TODO: Range tests follow -- should prolly create a suite for this
-    test("can parse left - dot", () => {
-        var cmd : node.CommandLine = parser.parse(".");
-        assert.equal(cmd.range.left[0].type, token.TokenType.Dot);
-    });
+	// TODO: Range tests follow -- should prolly create a suite for this
+	test("can parse left - dot", () => {
+		var cmd : node.CommandLine = parser.parse(".");
+		assert.equal(cmd.range.left[0].type, token.TokenType.Dot);
+	});
 
-    test("can parse left - dollar", () => {
-        var cmd : node.CommandLine = parser.parse("$");
-        assert.equal(cmd.range.left[0].type, token.TokenType.Dollar);
-    });
+	test("can parse left - dollar", () => {
+		var cmd : node.CommandLine = parser.parse("$");
+		assert.equal(cmd.range.left[0].type, token.TokenType.Dollar);
+	});
 
-    test("can parse left - percent", () => {
-        var cmd : node.CommandLine = parser.parse("%");
-        assert.equal(cmd.range.left[0].type, token.TokenType.Percent);
-    });
+	test("can parse left - percent", () => {
+		var cmd : node.CommandLine = parser.parse("%");
+		assert.equal(cmd.range.left[0].type, token.TokenType.Percent);
+	});
 
-    test("can parse separator - comma", () => {
-        var cmd : node.CommandLine = parser.parse(",");
-        assert.equal(cmd.range.separator.type, token.TokenType.Comma);
-    });
+	test("can parse separator - comma", () => {
+		var cmd : node.CommandLine = parser.parse(",");
+		assert.equal(cmd.range.separator.type, token.TokenType.Comma);
+	});
 
-    test("can parse right - dollar", () => {
-        var cmd : node.CommandLine = parser.parse(",$");
-        assert.equal(cmd.range.left.length, 0);
-        assert.equal(cmd.range.right.length, 1);
-        assert.equal(cmd.range.right[0].type, token.TokenType.Dollar, "unexpected token");
-    });
+	test("can parse right - dollar", () => {
+		var cmd : node.CommandLine = parser.parse(",$");
+		assert.equal(cmd.range.left.length, 0);
+		assert.equal(cmd.range.right.length, 1);
+		assert.equal(cmd.range.right[0].type, token.TokenType.Dollar, "unexpected token");
+	});
 });
