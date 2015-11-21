b863fe8a142ae8d11722cd1bc95aefd0c520cc7d
Adriaan Putter <adriaan.putter@gmail.com>
2015-11-21 10:26:42 +0200

    introduced motion classes for Normal mode implement very basic word move

--- src/mode/mode_command.ts
+++ src/mode/mode_command.ts
@@ -1,10 +1,14 @@
 import {ModeName, Mode} from './mode';
 import {showCmdLine} from './../cmd_line/main';
 import * as vscode from 'vscode';
+import * as Motion from '../motion/motion';
 
 export default class CommandMode extends Mode {
+    motionMappings = [];
+    
     constructor() {
         super(ModeName.Command);
+        this.registerMotionMappings();
     }
 
     ShouldBeActivated(key : string, currentMode : ModeName) : boolean {
@@ -18,22 +22,22 @@ export default class CommandMode extends Mode {
     HandleKeyEvent(key : string) : void {
         this.keyHistory.push(key);
 
+                
         switch (key) {
             case ':':
                 showCmdLine();
                 break;
-            case 'h':
-                vscode.commands.executeCommand("cursorLeft");
-                break;
-            case 'j':
-                vscode.commands.executeCommand("cursorDown");
-                break;
-            case 'k':
-                vscode.commands.executeCommand("cursorUp");
-                break;
-            case 'l':
-                vscode.commands.executeCommand("cursorRight");
-                break;
+            default:
+                var map = this.motionMappings[key];
+                if (map != null) map.execute();
         }
+    }
+    
+    registerMotionMappings() {
+        this.motionMappings['h'] = new Motion.MotionMoveLeft(); 
+        this.motionMappings['j'] = new Motion.MotionMoveDown(); 
+        this.motionMappings['k'] = new Motion.MotionMoveUp(); 
+        this.motionMappings['l'] = new Motion.MotionMoveRight();
+        this.motionMappings['w'] = new Motion.MotionMoveWordForward();
     }
 }
