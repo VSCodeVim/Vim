06dd05aa4008eedc819cbb0894be2eab4e19dc99
guillermooo <guillermo.lopez@outlook.com>
2015-11-25 22:32:03 +0100

    minor fixes

--- src/mode/mode_command.ts
+++ src/mode/mode_command.ts
@@ -5,51 +5,47 @@ import * as vscode from 'vscode';
 
 export default class CommandMode extends Mode {
     private keyHandler : { [key : string] : (position : vscode.Position) => void; } = {};
-    
+
     constructor() {
         super(ModeName.Command);
-        
+
         this.keyHandler = {
-            ":" : (position) => { showCmdLine() },   
-            "h" : (position) => { vscode.commands.executeCommand("cursorLeft"); },        
-            "j" : (position) => { vscode.commands.executeCommand("cursorDown"); },        
-            "k" : (position) => { vscode.commands.executeCommand("cursorUp"); },      
+            ":" : (position) => { showCmdLine(); },
+            "h" : (position) => { vscode.commands.executeCommand("cursorLeft"); },
+            "j" : (position) => { vscode.commands.executeCommand("cursorDown"); },
+            "k" : (position) => { vscode.commands.executeCommand("cursorUp"); },
             "l" : (position) => { vscode.commands.executeCommand("cursorRight"); },
-            ">>" : (position) => { vscode.commands.executeCommand("editor.action.indentLines"); },
-            "<<" : (position) => { vscode.commands.executeCommand("editor.action.outdentLines"); }, 
+            // XXX(guillermooo): renamed these commands for testing from >> and << because VSC can't handle '<'
+            "tt" : (position) => { vscode.commands.executeCommand("editor.action.indentLines"); },
+            "TT" : (position) => { vscode.commands.executeCommand("editor.action.outdentLines"); }
+            // ================================================================================================
         };
     }
 
     ShouldBeActivated(key : string, currentMode : ModeName) : boolean {
-        return (key === 'esc' || key == 'ctrl+[');
+        return (key === 'esc' || key === 'ctrl+[');
     }
 
     HandleActivation(key : string) : void {
         // do nothing
-    }    
+    }
 
     HandleKeyEvent(key : string) : void {
         this.keyHistory.push(key);
-        
+
         const editor = vscode.window.activeTextEditor;
         const currentPosition = editor.selection.active;
-        
-        //var commands = vscode.commands.getCommands();
-        //commands.then(a => console.log(a));
 
         var keyHandled = false;
-        for (var window = 1; window <= 2 && window <= this.keyHistory.length && keyHandled === false; window++) {
-            var keys : string  = _.takeRight(this.keyHistory, window).join('').toString();
-            
-            console.log(keys);
-            if (this.keyHandler[key] !== undefined) {
-                // Bug: 'two characters does not work'
-                console.log('handled');
-                keyHandled = true;
-                this.keyHandler[key](currentPosition);
+        
+        for (let window = 1; window <= 2 && window <= this.keyHistory.length && !keyHandled; window++) {
+            let keys  = _.takeRight(this.keyHistory, window).join('').toString();
+
+            if (keyHandled = this.keyHandler[keys] !== undefined) {
+                this.keyHandler[keys](currentPosition);
             }
         }
-        
+
         if (keyHandled) {
             this.keyHistory = [];
         }
