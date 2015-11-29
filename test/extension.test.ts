560875c075bc754b9c95e5e6574f37fb763c930b
Jason Poon <git@jasonpoon.ca>
2015-11-28 23:11:55 -0800

    * Extensions Refactor. * Add tests to ensure proper keybindings

---
+++ test/extension.test.ts
@@ -0,0 +1,36 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import * as _ from 'lodash';
+
+suite("setup", () => {
+	test("all keys have handlers", (done) => {
+		let pkg = require(__dirname + '/../../package.json');
+		assert.ok(pkg);
+		
+		vscode.commands.getCommands()
+			.then(registeredCommands => {
+				let keybindings = pkg.contributes.keybindings;
+				assert.ok(pkg);
+
+				for (let i = 0; i < keybindings.length; i++) {
+					let keybinding = keybindings[i];
+					
+					var found = registeredCommands.indexOf(keybinding.command) >= -1;
+					assert.ok(found, "Missing handler for key=" + keybinding.key + ". Expected handler=" + keybinding.command);
+				}
+			})
+			.then(done, done);
+	});
+	
+	test("duplicate keybindings", () => {
+		let pkg = require(__dirname + '/../../package.json');
+		assert.ok(pkg);
+
+		let keys = _.pluck(pkg.contributes.keybindings, "key");
+		let duplicateKeys = _.filter(keys, function(x, i, array) {
+			return _.includes(array, x, i + 1);
+		});
+		
+		assert.equal(duplicateKeys.length, 0, "Duplicate Keybindings: " + duplicateKeys.join(','));
+	});
+});
