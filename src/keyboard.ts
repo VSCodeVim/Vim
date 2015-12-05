c927d05561e8ef06ad611ffa91490a7c00109eb2
snowz <marcel.patzwahl@gmail.com>
2015-12-05 22:51:44 +0100

    tslint fix

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -67,7 +67,7 @@ class KeyMapperDeDeQwertz implements KeyMapper {
             '>': ':',
             '\\': '<',
             '<': ';',
-            '^': '&',
+            '^': '&'
         };
     }
 
