3b8c3b99c496df23c06cd09217a7dcd83cdccdad
Francisco Requena <frarees@gmail.com>
2016-02-29 11:51:42 +0100

    Fix infinite loop when workspace is empty

--- src/motion/position.ts
+++ src/motion/position.ts
@@ -254,12 +254,14 @@ export class Position extends vscode.Position {
         let positions = [];
 
         regex.lastIndex = 0;
-        while (true) {
-            let result = regex.exec(currentLine.text);
-            if (result === null) {
-                break;
+        if (currentLine.text) {
+            while (true) {
+                let result = regex.exec(currentLine.text);
+                if (result === null) {
+                    break;
+                }
+                positions.push(result.index);
             }
-            positions.push(result.index);
         }
 
         for (var index = 0; index < positions.length; index++) {
