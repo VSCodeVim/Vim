7827c9d5e24f1ba23f78c044d8c96b6f68ae494f
Tienshiao Ma <tienshiao@tienshiao.org>
2016-03-07 16:56:08 -0800

    Fix compile error.

--- test/mode/modeInsert.test.ts
+++ test/mode/modeInsert.test.ts
@@ -6,17 +6,21 @@ import {InsertMode} from '../../src/mode/modeInsert';
 import {ModeName} from '../../src/mode/mode';
 import {Motion, MotionMode} from '../../src/motion/motion';
 import {TextEditor} from '../../src/textEditor';
+import {ModeHandler} from '../../src/mode/modeHandler';
+
 
 suite("Mode Insert", () => {
 
     let motion: Motion;
     let modeInsert: InsertMode;
+    let modeHandler: ModeHandler;
 
     setup(async () => {
         await setupWorkspace();
 
         motion = new Motion(MotionMode.Cursor);
-        modeInsert = new InsertMode(motion);
+        modeHandler = new ModeHandler();
+        modeInsert = new InsertMode(motion, modeHandler);
     });
 
     teardown(cleanUpWorkspace);
