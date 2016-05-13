3843e54da9826739aa29c2ed8c19323936fa0899
Pieter Joost van de Sande <pj@born2code.net>
2016-05-13 10:01:31 +0200

    Add missing semicolon

--- src/textEditor.ts
+++ src/textEditor.ts
@@ -15,12 +15,12 @@ export class TextEditor {
             editBuilder.insert(position, text);
         });
     }
-    
+
     static async copy(range: vscode.Range): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            const text = vscode.window.activeTextEditor.document.getText(range); 
+            const text = vscode.window.activeTextEditor.document.getText(range);
             copy(text, (err) => {
-                (err) ? reject() : resolve()
+                (err) ? reject() : resolve();
             });
         });
     }
@@ -85,7 +85,7 @@ export class TextEditor {
     static getSelection(): vscode.Range {
         return vscode.window.activeTextEditor.selection;
     }
-    
+
     static isFirstLine(position : vscode.Position): boolean {
         return position.line === 0;
     }
