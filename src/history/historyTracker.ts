2fc08e2ca93df2d50ca3666010a5cd941b221db4
johnfn <johnfn@gmail.com>
2016-06-28 02:10:42 -0400

    Add documentation.

--- src/history/historyTracker.ts
+++ src/history/historyTracker.ts
@@ -1,3 +1,15 @@
+/**
+ * HistoryTracker is a handrolled undo/redo tracker for VSC. We currently
+ * track history as a list of "steps", each of which consists of 1 or more
+ * "changes".
+ *
+ * A Change is something like adding or deleting a few letters.
+ *
+ * A Step is multiple Changes.
+ *
+ * Undo/Redo will advance forward or backwards through Steps.
+ */
+
 import * as vscode from "vscode";
 
 import { Position } from './../motion/position';
@@ -78,7 +90,15 @@ export class HistoryTracker {
         this.oldText = TextEditor.getAllText();
     }
 
+    /**
+     * Adds an individual Change to the current Step.
+     *
+     * Determines what changed by diffing the document against what it
+     * used to look like.
+     */
     addChange(): void {
+        // Determine if we should add a new Step.
+
         if (this.currentHistoryStepIndex === -1 || (
                 this.currentHistoryStepIndex === this.historySteps.length - 1 &&
                 this.currentHistoryStep.isFinished)) {
@@ -116,10 +136,20 @@ export class HistoryTracker {
         this.oldText = newText;
     }
 
+    /**
+     * Tells the HistoryTracker that although the document has changed, we should simply
+     * ignore that change. Most often used when the change was itself triggered by
+     * the HistoryTracker.
+     */
     ignoreChange(): void {
         this.oldText = TextEditor.getAllText();
     }
 
+    /**
+     * Until we mark it as finished, the active Step will
+     * accrue multiple changes. This function will mark it as finished,
+     * and the next time we add a change, it'll be added to a new Step.
+     */
     finishCurrentStep(): void {
         if (this.currentHistoryStep.changes.length === 0) {
             return;
