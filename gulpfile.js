dc17dac122043fe7512a8a19091d4dc956b59472
Jason Poon <jpoon@users.noreply.github.com>
2016-07-08 14:40:48 -0700

    Include vscode typings (#419)

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp'),
     tslint = require('gulp-tslint'),
     typings = require('gulp-typings'),
     shell = require('gulp-shell'),
-    mocha = require('gulp-mocha'),
     soften = require('gulp-soften'),
     git = require('gulp-git'),
     bump = require('gulp-bump'),
     filter = require('gulp-filter'),
     tag_version = require('gulp-tag-version'),
+    inject = require('gulp-inject-string'),
     trimlines = require('gulp-trimlines');
 
 var paths = {
-    scripts_ts: "src/**/*.ts",
+    src_ts: "src/**/*.ts",
     tests_ts: "test/**/*.ts"
 };
 
@@ -33,11 +33,23 @@ gulp.task('typings', function () {
         .pipe(typings());
 });
 
+gulp.task('typings-vscode-definitions', ['typings'], function() {
+    // add vscode definitions
+    var vscodeTypings = '/// <reference path="../node_modules/vscode/typings/index.d.ts" />\n';
+    var vscodeNodeTypings = '/// <reference path="../node_modules/vscode/typings/node.d.ts" />\n';
+    return gulp.src('./typings/index.d.ts')
+        .pipe(inject.replace(vscodeTypings, ''))
+        .pipe(inject.replace(vscodeNodeTypings, ''))
+        .pipe(inject.prepend(vscodeTypings))
+        .pipe(inject.prepend(vscodeNodeTypings))
+        .pipe(gulp.dest('./typings'));
+})
+
 gulp.task('fix-whitespace', function() {
     // 1. change tabs to spaces
     // 2. trim trailing whitespace
-    return gulp.src([paths.scripts_ts, paths.tests_ts], { base: "./" })
-        .pipe(soften(4))
+    return gulp.src([paths.src_ts, paths.tests_ts], { base: "./" })
+        .pipe(soften(2))
         .pipe(trimlines({
             leading: false
         }))
@@ -45,10 +57,11 @@ gulp.task('fix-whitespace', function() {
 });
 
 gulp.task('tslint', ['fix-whitespace'], function() {
-    return gulp.src([paths.scripts_ts, paths.tests_ts])
+    return gulp.src([paths.src_ts, paths.tests_ts])
         .pipe(tslint())
         .pipe(tslint.report('prose', {
-          summarizeFailureOutput: true
+          summarizeFailureOutput: true,
+          emitError: false
         }));
 });
 
@@ -56,6 +69,6 @@ gulp.task('compile', shell.task([
   'node ./node_modules/vscode/bin/compile -p ./',
 ]));
 
-gulp.task('init', ['typings']);
+gulp.task('init', ['typings', 'typings-vscode-definitions']);
 gulp.task('default', ['tslint', 'compile']);
 gulp.task('release', ['default', 'patch']);
