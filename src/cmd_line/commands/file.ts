e06daabdd712c6d16124c28895fe079b15d68c70
Peng Lyu <penn.lv@gmail.com>
2016-07-16 12:23:40 +0800

    Open file in new window. (#404)
    * Open file in new window.
    
    This feature is limited to VS Code's group editors, which only support 3 groups at most.
    
    * Open file in new window.
    
    * Fix whitespace

--- src/cmd_line/commands/file.ts
+++ src/cmd_line/commands/file.ts
@@ -4,8 +4,14 @@ import * as vscode from "vscode";
 import * as path from "path";
 import * as node from "../node";
 
+export enum FilePosition {
+    CurrentWindow,
+    NewWindow
+}
+
 export interface IFileCommandArguments extends node.ICommandArgs {
     name?: string;
+    position?: FilePosition;
 }
 
 
@@ -23,13 +29,54 @@ export class FileCommand extends node.CommandBase {
         return this._arguments;
     }
 
-    execute(): void {
+    getActiveViewColumn(): vscode.ViewColumn {
+        const active = vscode.window.activeTextEditor;
+
+        if (!active) {
+            return vscode.ViewColumn.One;
+        }
+
+        return active.viewColumn;
+    }
+
+    getViewColumnToRight() : vscode.ViewColumn {
+        const active = vscode.window.activeTextEditor;
+
+        if (!active) {
+            return vscode.ViewColumn.One;
+        }
+
+        switch (active.viewColumn) {
+            case vscode.ViewColumn.One:
+                return vscode.ViewColumn.Two;
+            case vscode.ViewColumn.Two:
+                return vscode.ViewColumn.Three;
+        }
+
+        return active.viewColumn;
+    }
+
+    async execute(): Promise<void> {
+        if (!this._arguments.name) {
+            // Open an empty file
+            if (this._arguments.position === FilePosition.CurrentWindow) {
+                await vscode.commands.executeCommand("workbench.action.files.newUntitledFile");
+            } else {
+                await vscode.commands.executeCommand("workbench.action.splitEditor");
+                await vscode.commands.executeCommand("workbench.action.files.newUntitledFile");
+                await vscode.commands.executeCommand("workbench.action.closeOtherEditors");
+            }
+
+            return;
+        }
+
         let currentFilePath = vscode.window.activeTextEditor.document.uri.path;
         let newFilePath = path.join(path.dirname(currentFilePath), this._arguments.name);
 
         if (newFilePath !== currentFilePath) {
             let folder = vscode.Uri.file(newFilePath);
-            vscode.commands.executeCommand("vscode.open", folder);
+            await vscode.commands.executeCommand("vscode.open", folder,
+                this._arguments.position === FilePosition.NewWindow ? this.getViewColumnToRight() : this.getActiveViewColumn());
         }
     }
 }
--- src/cmd_line/subparsers/file.ts
+++ src/cmd_line/subparsers/file.ts
@@ -8,10 +8,31 @@ export function parseEditFileCommandArgs(args: string): node.FileCommand {
         return new node.FileCommand({});
     }
 
-    var scanner = new Scanner(args);
+    let scanner = new Scanner(args);
     let name = scanner.nextWord();
 
     return new node.FileCommand({
-        name: name
+        name: name,
+        position: node.FilePosition.CurrentWindow
+    });
+}
+
+export function parseEditNewFileInNewWindowCommandArgs(args: string): node.FileCommand {
+    return new node.FileCommand({
+        position: node.FilePosition.NewWindow
+    });
+}
+
+export function parseEditFileInNewWindowCommandArgs(args: string): node.FileCommand {
+    let name = "";
+
+    if (args) {
+        let scanner = new Scanner(args);
+        name = scanner.nextWord();
+    }
+
+    return new node.FileCommand({
+        name: name,
+        position: node.FilePosition.NewWindow
     });
 }
