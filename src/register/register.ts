f3ecd4056c99d2f37571bb3bcc19c613e7e715ce
amin roosta <amin.roosta@outlook.com>
2016-08-01 02:45:36 +0430

    support alphanameric registers & update roadmap

--- src/register/register.ts
+++ src/register/register.ts
@@ -19,16 +19,22 @@ export interface IRegisterContent {
 
 export class Register {
   /**
+   * The '"' is the unnamed register.
    * The '*' is the special register for stroing into system clipboard.
+   * TODO: Read-Only registers
+   *  '.' register has the last inserted text.
+   *  '%' register has the current file path.
+   *  ':' is the most recently executed command.
+   *  '#' is the name of last edited file. (low priority)
    */
-  private static validRegisters = [
-    '"',
-    '*'
-  ];
-
   private static registers: { [key: string]: IRegisterContent } = {
-    '"': { text: "", registerMode: RegisterMode.CharacterWise }
+    '"': { text: "", registerMode: RegisterMode.CharacterWise },
+    '*': { text: "", registerMode: RegisterMode.CharacterWise }
   };
+
+  private static isValidRegister(register: string): boolean {
+    return register in Register.registers || /^[a-z0-9]+$/i.test(register);
+  }
 
   /**
    * Puts content in a register. If none is specified, uses the default
@@ -37,7 +43,7 @@ export class Register {
   public static put(content: string, vimState: VimState): void {
     const register = vimState.recordedState.registerName;
 
-    if (Register.validRegisters.indexOf(register) === -1) {
+    if (!Register.isValidRegister(register)) {
       throw new Error(`Invalid register ${register}`);
     }
 
@@ -58,8 +64,12 @@ export class Register {
   public static async get(vimState: VimState): Promise<IRegisterContent> {
     const register = vimState.recordedState.registerName;
 
-    if (Register.validRegisters.indexOf(register) === -1) {
+    if (!Register.isValidRegister(register)) {
       throw new Error(`Invalid register ${register}`);
+    }
+
+    if (!Register.registers[register]) {
+      Register.registers[register] = { text: "", registerMode: RegisterMode.CharacterWise };
     }
 
     /* Read from system clipboard */
