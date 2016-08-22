5576591d0fb378398340d7a377b8b1e982a57d5c
Lukas Platzer <lukas.platzer@delfortgroup.com>
2016-08-22 10:06:37 +0200

    created writequit test

---
+++ test/cmd_line/writequit.test.ts
@@ -0,0 +1,58 @@
+"use strict";
+
+import { ModeHandler } from '../../src/mode/modeHandler';
+import { setupWorkspace, cleanUpWorkspace, assertEqualLines, assertEqual } from './../testUtils';
+import { runCmdLine } from '../../src/cmd_line/main';
+import * as vscode from "vscode";
+import {join} from 'path';
+import * as assert from 'assert';
+
+suite("Basic write-quit", () => {
+  let modeHandler: ModeHandler;
+
+  setup(async () => {
+    await setupWorkspace();
+    modeHandler = new ModeHandler();
+  });
+
+  teardown(cleanUpWorkspace);
+
+  test("Run write and quit", async () => {
+    await modeHandler.handleMultipleKeyEvents(['i', 'a', 'b', 'a', '<escape>']);
+    // await runCmdLine("%s/a/d", modeHandler);
+    await runCmdLine("wq", modeHandler);
+
+    // cleanUpWorkspace - testUtils.ts
+    let poll = new Promise((c, e) => {
+      if (vscode.window.visibleTextEditors.length === 0) {
+        return c();
+      }
+
+      let pollCount = 0;
+      // TODO: the visibleTextEditors variable doesn't seem to be
+      // up to date after a onDidChangeActiveTextEditor event, not
+      // even using a setTimeout 0... so we MUST poll :(
+      let interval = setInterval(() => {
+        // if visibleTextEditors is not updated after 1 sec
+        // we can expect that 'wq' failed
+        if (pollCount <= 100) {
+          pollCount++;
+          if (vscode.window.visibleTextEditors.length > 0) {
+            return;
+          }
+        }
+
+        clearInterval(interval);
+        c();
+      }, 10);
+    });
+
+    try {
+      await poll;
+    } catch (error) {
+      assert.fail(null, null, error.toString());
+    }
+
+    assertEqual(vscode.window.visibleTextEditors.length, 0, "Window after 1sec still open");
+  });
+});
