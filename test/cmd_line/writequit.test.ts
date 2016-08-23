9673a52df27890c6ce5ef09a8293875e41061cb2
Lukas Platzer <lukas.platzer@delfortgroup.com>
2016-08-23 08:24:52 +0200

    extract polling for vscode into function

--- test/cmd_line/writequit.test.ts
+++ test/cmd_line/writequit.test.ts
@@ -7,6 +7,39 @@ import * as vscode from "vscode";
 import {join} from 'path';
 import * as assert from 'assert';
 
+async function WaitForVsCodeClose() : Promise<void> {
+  // cleanUpWorkspace - testUtils.ts
+  let poll = new Promise((c, e) => {
+    if (vscode.window.visibleTextEditors.length === 0) {
+      return c();
+    }
+
+    let pollCount = 0;
+    // TODO: the visibleTextEditors variable doesn't seem to be
+    // up to date after a onDidChangeActiveTextEditor event, not
+    // even using a setTimeout 0... so we MUST poll :(
+    let interval = setInterval(() => {
+      // if visibleTextEditors is not updated after 1 sec
+      // we can expect that 'wq' failed
+      if (pollCount <= 100) {
+        pollCount++;
+        if (vscode.window.visibleTextEditors.length > 0) {
+          return;
+        }
+      }
+
+      clearInterval(interval);
+      c();
+    }, 10);
+  });
+
+  try {
+    await poll;
+  } catch (error) {
+    assert.fail(null, null, error.toString());
+  }
+}
+
 suite("Basic write-quit", () => {
   let modeHandler: ModeHandler;
 
@@ -19,39 +52,9 @@ suite("Basic write-quit", () => {
 
   test("Run write and quit", async () => {
     await modeHandler.handleMultipleKeyEvents(['i', 'a', 'b', 'a', '<escape>']);
-    // await runCmdLine("%s/a/d", modeHandler);
+
     await runCmdLine("wq", modeHandler);
-
-    // cleanUpWorkspace - testUtils.ts
-    let poll = new Promise((c, e) => {
-      if (vscode.window.visibleTextEditors.length === 0) {
-        return c();
-      }
-
-      let pollCount = 0;
-      // TODO: the visibleTextEditors variable doesn't seem to be
-      // up to date after a onDidChangeActiveTextEditor event, not
-      // even using a setTimeout 0... so we MUST poll :(
-      let interval = setInterval(() => {
-        // if visibleTextEditors is not updated after 1 sec
-        // we can expect that 'wq' failed
-        if (pollCount <= 100) {
-          pollCount++;
-          if (vscode.window.visibleTextEditors.length > 0) {
-            return;
-          }
-        }
-
-        clearInterval(interval);
-        c();
-      }, 10);
-    });
-
-    try {
-      await poll;
-    } catch (error) {
-      assert.fail(null, null, error.toString());
-    }
+    await WaitForVsCodeClose();
 
     assertEqual(vscode.window.visibleTextEditors.length, 0, "Window after 1sec still open");
   });
