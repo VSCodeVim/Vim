782740da69b092a367f52a05301f0853292108d7
xconverge <sevendaystillsean@gmail.com>
2016-09-05 22:23:50 -0700

    added visual block tests

--- test/mode/modeVisualBlock.test.ts
+++ test/mode/modeVisualBlock.test.ts
@@ -6,54 +6,54 @@ import { ModeHandler } from '../../src/mode/modeHandler';
 import { getTestingFunctions } from '../testSimplifier';
 
 suite("Mode Visual Block", () => {
-    let modeHandler: ModeHandler = new ModeHandler();
+  let modeHandler: ModeHandler = new ModeHandler();
 
-    let {
-        newTest,
-        newTestOnly,
-    } = getTestingFunctions(modeHandler);
+  let {
+    newTest,
+    newTestOnly,
+  } = getTestingFunctions(modeHandler);
 
-    setup(async () => {
-        await setupWorkspace();
-        setTextEditorOptions(4, false);
-    });
+  setup(async () => {
+    await setupWorkspace();
+    setTextEditorOptions(4, false);
+  });
 
-    teardown(cleanUpWorkspace);
+  teardown(cleanUpWorkspace);
 
-    test("can be activated", async () => {
-        await modeHandler.handleKeyEvent('<C-v>');
-        assertEqual(modeHandler.currentMode.name, ModeName.VisualBlock);
+  test("can be activated", async () => {
+    await modeHandler.handleKeyEvent('<C-v>');
+    assertEqual(modeHandler.currentMode.name, ModeName.VisualBlock);
 
-        await modeHandler.handleKeyEvent('<C-v>');
-        assertEqual(modeHandler.currentMode.name, ModeName.Normal);
-    });
+    await modeHandler.handleKeyEvent('<C-v>');
+    assertEqual(modeHandler.currentMode.name, ModeName.Normal);
+  });
 
-    newTest({
-      title: "Can handle A forward select",
-      start: ['|test','test'],
-      keysPressed: 'l<C-v>ljA123',
-      end: ['tes123t','tes123|t'],
-    });
+  newTest({
+    title: "Can handle A forward select",
+    start: ['|test', 'test'],
+    keysPressed: 'l<C-v>ljA123',
+    end: ['tes123t', 'tes123|t'],
+  });
 
-    newTest({
-      title: "Can handle A backwards select",
-      start: ['tes|t','test'],
-      keysPressed: 'h<C-v>hjA123',
-      end: ['tes123t','tes123|t'],
-    });
+  newTest({
+    title: "Can handle A backwards select",
+    start: ['tes|t', 'test'],
+    keysPressed: 'h<C-v>hjA123',
+    end: ['tes123t', 'tes123|t'],
+  });
 
-    newTest({
-      title: "Can handle I forward select",
-      start: ['|test','test'],
-      keysPressed: 'l<C-v>ljI123',
-      end: ['t123est','t123|est'],
-    });
+  newTest({
+    title: "Can handle I forward select",
+    start: ['|test', 'test'],
+    keysPressed: 'l<C-v>ljI123',
+    end: ['t123est', 't123|est'],
+  });
 
-     newTest({
-      title: "Can handle I backwards select",
-      start: ['tes|t','test'],
-      keysPressed: 'h<C-v>hjI123',
-      end: ['t123est','t123|est'],
-    });
+  newTest({
+    title: "Can handle I backwards select",
+    start: ['tes|t', 'test'],
+    keysPressed: 'h<C-v>hjI123',
+    end: ['t123est', 't123|est'],
+  });
 
 });
