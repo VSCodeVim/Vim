3088db264a67a6c16482e47a8a55460c30da206e
Dominic Gee <dgee31@gmail.com>
2016-09-09 15:59:28 +0200

    Fixed tslint error

--- src/register/register.ts
+++ src/register/register.ts
@@ -61,7 +61,7 @@ export class Register {
         if (typeof existingText === 'string') {
           content = existingText + (existingText.endsWith('\n') ? '' : '\n') + content;
         } else {
-          content = existingText.concat(content.split('\n'))
+          content = existingText.concat(content.split('\n'));
         }
       } else {
         if (typeof existingText === 'string') {
