3e3e7f13dca8e6598d200f49eec872add3771e53
rebornix <penn.lv@gmail.com>
2016-12-02 15:09:54 -0800

    remove used import

--- src/state/searchState.ts
+++ src/state/searchState.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as _ from 'lodash';
 import { Position } from './../motion/position';
 import { TextEditor } from './../textEditor';
 import { Configuration } from '../../src/configuration/configuration';
