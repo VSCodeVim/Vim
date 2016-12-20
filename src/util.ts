341fca3103e4f97444ca9afb498ecfe8ba8a09bb
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2016-12-20 10:21:08 +0530

    Added util helper that waits for tab changes

--- src/util.ts
+++ src/util.ts
@@ -30,6 +30,23 @@ export async function waitForCursorUpdatesToHappen(): Promise<void> {
   });
 }
 
+/**
+ * FOR TESTING PURPOSES ONLY
+ * Waits for the tabs to change after a command like 'gt' or 'gT' is run.
+ * Sometimes it is not immediate, so we must busy wait
+ * Caveat: Use only it tests. This promise does not timeout. Awaiting this promise
+ * in extension code is dangerous
+ */
+export async function waitForTabChange(): Promise<void> {
+  await new Promise((resolve, reject) => {
+    const disposer = vscode.window.onDidChangeActiveTextEditor((textEditor) => {
+      disposer.dispose();
+
+      resolve(textEditor);
+    });
+  });
+}
+
 export async function allowVSCodeToPropagateCursorUpdatesAndReturnThem(): Promise<Range[]> {
   await waitForCursorUpdatesToHappen();
 
