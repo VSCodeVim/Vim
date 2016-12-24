034221e05c7fa92dc427235b6fd97dfab7626b25
xconverge <sevendaystillsean@gmail.com>
2016-12-23 20:02:21 -0800

    More surround helper functions added

--- src/surround/surround.ts
+++ src/surround/surround.ts
@@ -31,20 +31,49 @@ export class Surround {
   private static getSurroundPair(char: string): Pair | undefined {
     if (this.surroundPairs[char].pair !== undefined) {
       return this.surroundPairs[char].pair;
-    } else if (char !== "a") {
-      // TODO xconverge: check if char is not a letter, create a pair if it is not
+    } else if (!/[a-z]/i.test(char)) {
       return new Pair(char, char);
     } else {
       return undefined;
     }
   }
 
-  public static CSurroundHandler(charFrom: string, charTo: string, vimState: VimState){
+  private static inputTagPair(tagInput: string): Pair | undefined {
+    let tagName = "";
+    if (tagInput.endsWith(">")) {
+      tagName = tagInput.substring(0, tagInput.length - 1);
+      return new Pair("<" + tagName + ">", "</" + tagName + ">");
+    } else {
+      return undefined;
+    }
+  }
 
+  private static getOrInputPair(char: string): Pair | undefined {
+    return char === '<' || char === 't' ? this.inputTagPair(char) : this.getSurroundPair(char);
+  }
+
+
+
+  private static pasteSurround(innerValue: string, vimState: VimState): void {
 
   }
 
-  public static pasteSurround(innerValue: string, vimState: VimState): void {
+  private static change(charFrom: string, charTo: Pair, vimState: VimState) {
+
+  }
+
+  public static cSurroundHandler(charFrom: string, charTo: string, vimState: VimState){
+    const newSurround = this.getOrInputPair(charTo);
+    if (newSurround !== undefined) {
+      this.change(charFrom, newSurround, vimState);
+    }
+  }
+
+  public static dSurroundHandler(charFrom: string, charTo: string, vimState: VimState){
+    this.change(charFrom, new Pair("", ""), vimState);
+  }
+
+  public static ySurroundHandler(charFrom: string, charTo: string, vimState: VimState){
 
   }
 
