ab726144801d4301b515b11a57db7f8b2e21ca54
Horace He <horacehe2007@yahoo.com>
2017-04-23 17:42:54 -0400

    Fixes #1539

--- src/cmd_line/commands/register.ts
+++ src/cmd_line/commands/register.ts
@@ -26,15 +26,14 @@ export class RegisterCommand extends node.CommandBase {
     if (result instanceof Array) {
       result = result.join("\n").substr(0, 100);
     } else if (result instanceof RecordedState) {
-      // TODO
-      result = "";
+      result = result.actionsRun.map(x => x.keysPressed.join("")).join("");
     }
 
     return result;
   }
 
   async displayRegisterValue(register: string): Promise<void> {
-    let result = this.getRegisterDisplayValue(register);
+    let result = await this.getRegisterDisplayValue(register);
 
     vscode.window.showInformationMessage(`${register} ${result}`);
   }
