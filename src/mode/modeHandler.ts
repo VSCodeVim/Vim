31071993c509e5ce11f6cdf79ee0bfac5ce5085b
Horace He <horacehe2007@yahoo.com>
2017-04-25 22:36:40 -0400

    Fixes #1492

--- src/mode/modeHandler.ts
+++ src/mode/modeHandler.ts
@@ -826,7 +826,7 @@ export class ModeHandler implements vscode.Disposable {
     let previousWasCount = true;
 
     for (const commandKey of commandList) {
-      if (previousWasCount && commandKey.match(/[0-9]/)) {
+      if (previousWasCount && commandKey.match(/[1-9]/)) {
         continue;
       } else {
         previousWasCount = false;
