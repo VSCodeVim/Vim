a2bf075b46540db39a50cbab65a449f9978ca30b
Horace He <horacehe2007@yahoo.com>
2017-05-20 00:26:37 -0400

    Only uses neovim when command doesn't exist or we can substitute

--- src/cmd_line/main.ts
+++ src/cmd_line/main.ts
@@ -11,18 +11,18 @@ import { Configuration } from '../configuration/configuration';
 
 async function run(vimState: VimState, command: string) {
   const proc = spawn('nvim', ['-u', 'NONE', '-N', '--embed'], {cwd: __dirname });
-
   const nvim = await attach(proc.stdin, proc.stdout);
 
   const buf = await nvim.getCurrentBuf();
-  const window = await nvim.getCurrentWin();
   await buf.setLines(0, -1, true, TextEditor.getText().split('\n'));
+
   await nvim.callFunction("setpos", [".", [0, vimState.cursorPosition.line + 1, vimState.cursorPosition.character, false]]);
   await nvim.callFunction("setpos", ["'>", [0, vimState.cursorPosition.line + 1, vimState.cursorPosition.character, false]]);
   await nvim.callFunction("setpos", ["'<", [0, vimState.cursorStartPosition.line + 1, vimState.cursorStartPosition.character, false]]);
   for (const mark of vimState.historyTracker.getMarks()){
     await nvim.callFunction("setpos", [`'${mark.name}`, [0, mark.position.line + 1, mark.position.character, false]]);
   }
+
   await nvim.command(command);
 
   await TextEditor.replace(new vscode.Range(0, 0, TextEditor.getLineCount() - 1, TextEditor.getLineMaxColumn(TextEditor.getLineCount() - 1)), (await buf.getLines(0, -1, false)).join('\n'));
@@ -31,6 +31,7 @@ async function run(vimState: VimState, command: string) {
     await vscode.commands.executeCommand("editor.action.indentationToSpaces");
   }
   nvim.quit();
+  proc.kill();
   return;
 
 }
@@ -53,7 +54,6 @@ export async function showCmdLine(initialText: string, modeHandler : ModeHandler
   try {
     const cmdString = await vscode.window.showInputBox(options);
     await runCmdLine(cmdString!, modeHandler);
-    console.log("DONE");
     return;
   } catch (e) {
     modeHandler.setStatusBarText(e.toString());
@@ -66,22 +66,22 @@ export async function runCmdLine(command : string, modeHandler : ModeHandler) : 
     return;
   }
 
-  await run(modeHandler.vimState, command).then(() => {
-    console.log("SUCCESS");
-  }).catch((err)=>console.log(err));
-  return;
-  // try {
-  //   var cmd = parser.parse(command);
-  //   if (cmd.isEmpty) {
-  //     return;
-  //   }
-
-  //   await cmd.execute(modeHandler.vimState.editor, modeHandler);
-  //   return;
-  // } catch (e) {
-  //   await run(modeHandler, command).then(() => {
-  //     console.log("SUCCESS");
-  //   });
-  //   return;
-  // }
+  try {
+    var cmd = parser.parse(command);
+    if (cmd.isEmpty) {
+      return;
+    }
+    if (cmd.command.neovimCapable) {
+      await run(modeHandler.vimState, command).then(() => {
+        console.log("Substituted for neovim command");
+      }).catch((err) => console.log(err));
+    }
+    await cmd.execute(modeHandler.vimState.editor, modeHandler);
+    return;
+  } catch (e) {
+    await run(modeHandler.vimState, command).then(() => {
+      console.log("SUCCESS");
+    }).catch((err) => console.log(err));
+    return;
+  }
 }
