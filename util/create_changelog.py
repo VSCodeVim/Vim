c9b8b84dba60de8801f2d0d01baa00c3572a5060
Horace He <horacehe2007@yahoo.com>
2017-05-24 21:45:04 -0400

    Added changelog generator

--- util/create_changelog.py
+++ util/create_changelog.py
@@ -4,9 +4,11 @@ import re
 
 GITHUB_URL = "https://github.com"
 f = open("CHANGELOG.md", "w")
-r = requests.get('https://github.com/VSCodeVim/Vim/releases');
-bs = bs4.BeautifulSoup(r.text, "html5lib");
-results = bs.select('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.release-timeline > div')
+r = requests.get('https://github.com/VSCodeVim/Vim/releases')
+bs = bs4.BeautifulSoup(r.text, "html5lib")
+results = bs.select(
+    '#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.release-timeline > div'
+)
 for i in results:
   title = i.select_one("div.release-body.commit.open > div.release-header > h1 > a").text
   link = i.select_one("div.release-body.commit.open > div.release-header > h1 > a")["href"]
@@ -14,11 +16,22 @@ for i in results:
   date = i.select_one("div.release-body.commit.open > div.release-header > p > relative-time")
   date = date.text
   body = i.select_one("div.release-body.commit.open > div.markdown-body")
-  f.write("## [" + tag + " " +title+"]("+GITHUB_URL+link+")"+"  ("+date+")\n")
+  f.write("# [" + tag + " " + title + "](" + GITHUB_URL + link + ")" + "  (" + date + ")\n")
   # objects are all the paragraphs, headers, tags, etc.
   objects = body.select("p,h2,li")
+
+  objects = sorted(objects, key=lambda a: str(body).index(str(a)))
   for i in objects:
+    obj_text = i.decode_contents()
+    links = i.select('a')
+    for link in links:
+      obj_text.replace(str(link), link.text)
     if re.match('^<p>', str(i)):
-      f.write(i.text+'\n\n')
+      print('p')
+      f.write('\n' + obj_text + '\n')
     elif re.match('<li>', str(i)):
-      f.write('* '+i.text + '\n')
+      f.write('* ' + obj_text + '\n')
+      print('l')
+    elif re.match('<h2>', str(i)):
+      f.write('## ' + obj_text + '\n')
+  print('---------')
