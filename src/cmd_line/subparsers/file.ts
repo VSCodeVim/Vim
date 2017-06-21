1aed77996a81d7e3aa5a8c1f407882014fcc25b5
Horace He <horacehe2007@yahoo.com>
2017-06-21 14:02:42 -0700

    Properly added e!

--- src/cmd_line/subparsers/file.ts
+++ src/cmd_line/subparsers/file.ts
@@ -5,30 +5,30 @@ import {Scanner} from '../scanner';
 
 export function parseEditFileCommandArgs(args: string): node.FileCommand {
   if (!args) {
-    return new node.FileCommand({});
+    return new node.FileCommand({name: ""});
   }
 
   let scanner = new Scanner(args);
   let bang;
   const c = scanner.next();
-  if (c === '!') {
-    bang = true;
-  }
+  bang = (c === '!');
   if (scanner.isAtEof) {
-    return new node.FileCommand({bang: true});
+    return new node.FileCommand({name: "", bang: bang});
   }
 
   let name = scanner.nextWord();
 
   return new node.FileCommand({
-    name: name,
+    name: name.trim(),
     position: node.FilePosition.CurrentWindow,
-    bang: true
+    bang: bang
   });
 }
 
+// Note that this isn't really implemented.
 export function parseEditNewFileInNewWindowCommandArgs(args: string): node.FileCommand {
   return new node.FileCommand({
+    name: undefined,
     position: node.FilePosition.NewWindow
   });
 }
