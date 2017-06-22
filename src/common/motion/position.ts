d7c4503f4e2011d2fda1e6c64357b5f60ecb5df6
Horace He <horacehe2007@yahoo.com>
2017-06-22 01:59:05 -0700

    Fixed issues with some selections

--- src/common/motion/position.ts
+++ src/common/motion/position.ts
@@ -884,7 +884,7 @@ export class Position extends vscode.Position {
               (index >= this.character &&  inclusive)) || currentLine !== this.line);
 
       if (newCharacter !== undefined) {
-        return new Position(currentLine, newCharacter);
+        return new Position(currentLine, newCharacter + 1);
       }
     }
 
