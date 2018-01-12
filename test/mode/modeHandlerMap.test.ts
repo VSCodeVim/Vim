c05006620562db74c43aa73f7a9adb14073ebcb6
Jason Poon <git@jasonpoon.ca>
2018-01-12 03:20:51 -0800

    refactor: move modehandlermap to own class

---
+++ test/mode/modeHandlerMap.test.ts
@@ -0,0 +1,42 @@
+import * as assert from 'assert';
+
+import { getAndUpdateModeHandler } from '../../extension';
+import { ModeName } from '../../src/mode/mode';
+import { ModeHandlerMap } from '../../src/mode/modeHandlerMap';
+import { cleanUpWorkspace, setupWorkspace } from './../testUtils';
+
+suite('Mode Handler Map', () => {
+  setup(() => {
+    ModeHandlerMap.clear();
+  });
+
+  teardown(() => {
+    ModeHandlerMap.clear();
+  });
+
+  test('getOrCreate', async () => {
+    // getOrCreate
+    let key = Math.random()
+      .toString(36)
+      .substring(7);
+    let [modeHandler, isNew] = await ModeHandlerMap.getOrCreate(key);
+    assert.equal(isNew, true);
+    assert.notEqual(modeHandler, undefined);
+
+    [, isNew] = await ModeHandlerMap.getOrCreate(key);
+    assert.equal(isNew, false);
+
+    // getKeys
+    let keys = ModeHandlerMap.getKeys();
+    assert.equal(keys.length, 1);
+    assert.equal(keys[0], key);
+
+    // getAll
+    let modeHandlerList = ModeHandlerMap.getAll();
+    assert.equal(modeHandlerList.length, 1);
+
+    // delete
+    ModeHandlerMap.delete(key);
+    assert.equal(ModeHandlerMap.getAll().length, 0);
+  });
+});
