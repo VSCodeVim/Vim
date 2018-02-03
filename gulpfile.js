9bc9213c72bb09140cc19f9bf5e94503d35f0232
Jason Poon <git@jasonpoon.ca>
2018-02-03 02:07:57 -0800

    Add: 'gulp test' which will run UTs inside a container. Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,32 @@
 var gulp = require('gulp'),
   bump = require('gulp-bump'),
-  git = require('gulp-git'),
-  inject = require('gulp-inject-string'),
-  merge = require('merge-stream'),
-  tag_version = require('gulp-tag-version'),
-  ts = require('gulp-typescript'),
-  tslint = require('gulp-tslint'),
   filter = require('gulp-filter'),
-  shell = require('gulp-shell'),
-  exec = require('child_process').exec;
+  git = require('gulp-git'),
+  tag_version = require('gulp-tag-version'),
+  tslint = require('gulp-tslint'),
+  ts = require('gulp-typescript');
 
-var paths = {
-  src_ts: 'src/**/*.ts',
-  tests_ts: 'test/**/*.ts',
-};
-
-function versionBump(semver) {
-  return gulp
-    .src(['./package.json', './package-lock.json'])
-    .pipe(bump({ type: semver }))
-    .pipe(gulp.dest('./'))
-    .pipe(git.commit('bump version'))
-    .pipe(filter('package.json'))
-    .pipe(tag_version());
-}
-
+// compile
 gulp.task('compile', function(){
   var tsProject = ts.createProject('./tsconfig.json');
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('out'));
 });
 
+// tslint
 gulp.task('tslint', function() {
-  var tslintOptions = {
-    summarizeFailureOutput: true,
-  };
-
-  var srcs = gulp
-    .src(paths.src_ts)
+  return gulp
+    .src([
+      '**/*.ts',
+      '!node_modules/**',
+      '!typings/**',
+    ])
     .pipe(tslint({ formatter: 'verbose' }))
-    .pipe(tslint.report(tslintOptions));
-  var tests = gulp
-    .src(paths.tests_ts)
-    .pipe(tslint({ formatter: 'verbose' }))
-    .pipe(tslint.report(tslintOptions));
-  return merge(srcs, tests);
+    .pipe(tslint.report({ summarizeFailureOutput: true }));
 });
 
-gulp.task('prettier', function() {
-  runPrettier('git diff --name-only HEAD');
-});
-
-gulp.task('forceprettier', function() {
-  // files managed by git
-  runPrettier('git ls-files');
-  // untracked files
-  runPrettier('git ls-files --others --exclude-standard');
-});
-
+// prettier
 function runPrettier(command) {
+  let exec = require('child_process').exec;
   exec(command, function(err, stdout, stderr) {
     const files = stdout.split('\n');
     for (const file of files) {
@@ -70,14 +39,71 @@ function runPrettier(command) {
   });
 }
 
-gulp.task('default', ['prettier', 'tslint', 'compile']);
+gulp.task('prettier', function() {
+  runPrettier('git diff --name-only HEAD');
+});
+
+gulp.task('forceprettier', function() {
+  // files managed by git
+  runPrettier('git ls-files');
+  // untracked files
+  runPrettier('git ls-files --others --exclude-standard');
+});
+
+
+// test
+gulp.task('test', function(cb) {
+  let spawn = require('child_process').spawn;
+
+  const dockerTag = 'vscodevim'
+
+  console.log("Building container...")
+  let dockerBuildCmd = spawn('docker', ['build', '-f', './build/Dockerfile', '.', '-t', dockerTag], {
+    cwd : process.cwd(),
+    stdio: 'inherit',
+  });
+
+  dockerBuildCmd.on('exit', function (code) {
+    if (code !== 0) {
+      cb(code);
+      return;
+    }
+
+    console.log("Running tests inside container...")
+    let dockerRunCmd = spawn('docker', ['run', '-v', '$PWD:/app', dockerTag], {
+      cwd : process.cwd(),
+      stdio: 'inherit',
+      env: process.env,
+      shell: true,
+    });
+
+    dockerRunCmd.on('exit', function (code) {
+      cb(code);
+    });
+  });
+});
+
+// version bump
+function versionBump(semver) {
+  return gulp
+    .src(['./package.json', './package-lock.json'])
+    .pipe(bump({ type: semver }))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('bump version'))
+    .pipe(filter('package.json'))
+    .pipe(tag_version());
+}
 
 gulp.task('patch', ['default'], function() {
   return versionBump('patch');
 });
+
 gulp.task('minor', ['default'], function() {
   return versionBump('minor');
 });
+
 gulp.task('major', ['default'], function() {
   return versionBump('major');
 });
+
+gulp.task('default', ['prettier', 'tslint', 'compile']);
