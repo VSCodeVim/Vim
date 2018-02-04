bd652f00fec8a5b329bb2a874b0fe5702b1031eb
Jason Poon <git@jasonpoon.ca>
2018-02-04 03:46:50 -0800

    fix: fix pathing for sourcemaps. fix errors with prettier gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -8,79 +8,84 @@ var gulp = require('gulp'),
   ts = require('gulp-typescript');
 
 // compile
-gulp.task('compile', function(){
-  var tsProject = ts.createProject('./tsconfig.json');
-  return tsProject.src()
+gulp.task('compile', function() {
+  var tsProject = ts.createProject('tsconfig.json');
+  return tsProject
+    .src()
     .pipe(sourcemaps.init())
     .pipe(tsProject())
-    .pipe(sourcemaps.write())
+    .js.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '' }))
     .pipe(gulp.dest('out'));
 });
 
 // tslint
 gulp.task('tslint', function() {
   return gulp
-    .src([
-      '**/*.ts',
-      '!node_modules/**',
-      '!typings/**',
-    ])
+    .src(['**/*.ts', '!node_modules/**', '!typings/**'])
     .pipe(tslint({ formatter: 'prose' }))
     .pipe(tslint.report({ summarizeFailureOutput: true }));
 });
 
 // prettier
-function runPrettier(command) {
+function runPrettier(command, cb) {
   var exec = require('child_process').exec;
   exec(command, function(err, stdout, stderr) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
     const files = stdout.split('\n');
     for (const file of files) {
       if (file.endsWith('.ts') || file.endsWith('.js')) {
         exec(
-          `node ./node_modules/prettier/bin/prettier.js --write --print-width 100 --single-quote --trailing-comma es5 ${file}`
+          `node ./node_modules/.bin/prettier --write --print-width 100 --single-quote --trailing-comma es5 ${file}`,
+          cb
         );
       }
     }
   });
 }
 
-gulp.task('prettier', function() {
-  runPrettier('git diff --name-only HEAD');
+gulp.task('prettier', function(cb) {
+  // files changed
+  runPrettier('git diff --name-only HEAD', cb);
 });
 
-gulp.task('forceprettier', function() {
+gulp.task('forceprettier', function(cb) {
   // files managed by git
-  runPrettier('git ls-files');
-  // untracked files
-  runPrettier('git ls-files --others --exclude-standard');
+  runPrettier('git ls-files', cb);
 });
-
 
 // test
 gulp.task('test', function(cb) {
   var spawn = require('child_process').spawn;
-  const dockerTag = 'vscodevim'
+  const dockerTag = 'vscodevim';
 
-  console.log("Building container...")
-  var dockerBuildCmd = spawn('docker', ['build', '-f', './build/Dockerfile', '.', '-t', dockerTag], {
-    cwd : process.cwd(),
-    stdio: 'inherit',
-  });
+  console.log('Building container...');
+  var dockerBuildCmd = spawn(
+    'docker',
+    ['build', '-f', './build/Dockerfile', '.', '-t', dockerTag],
+    {
+      cwd: process.cwd(),
+      stdio: 'inherit',
+    }
+  );
 
-  dockerBuildCmd.on('exit', function (code) {
+  dockerBuildCmd.on('exit', function(code) {
     if (code !== 0) {
       cb(code);
       return;
     }
 
-    console.log("Running tests inside container...")
-    console.log("To break, run `docker kill` in a separate terminal.")
+    console.log('Running tests inside container...');
+    console.log('To break, run `docker kill` in a separate terminal.');
     var dockerRunCmd = spawn('docker', ['run', '-v', process.cwd() + ':/app', dockerTag], {
-      cwd : process.cwd(),
+      cwd: process.cwd(),
       stdio: 'inherit',
     });
 
-    dockerRunCmd.on('exit', function (code) {
+    dockerRunCmd.on('exit', function(code) {
       cb(code);
     });
   });
