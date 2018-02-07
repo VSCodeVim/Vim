a2023fd84c315f4ac07a87f748381b856de258bf
Jason Poon <git@jasonpoon.ca>
2018-02-06 19:59:08 -0800

    refactor: remove lastClickWasPastEol from vim state

--- src/state/vimState.ts
+++ src/state/vimState.ts
@@ -173,11 +173,6 @@ export class VimState implements vscode.Disposable {
   public lastVisualSelectionEnd: Position;
 
   /**
-   * Was the previous mouse click past EOL
-   */
-  public lastClickWasPastEol: boolean = false;
-
-  /**
    * The mode Vim will be in once this action finishes.
    */
   private _currentMode: ModeName;
