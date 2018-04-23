1c81cf3e657320a015e911ceee3fd07b0a6dda4c
Horace He <horacehe2007@yahoo.com>
2018-04-23 15:15:20 -0400

    Removed viewcolumn from editoridentity information

--- src/editorIdentity.ts
+++ src/editorIdentity.ts
@@ -2,30 +2,25 @@ import * as vscode from 'vscode';
 
 export class EditorIdentity {
   private _fileName: string;
-  private _viewColumn: vscode.ViewColumn;
 
   constructor(textEditor?: vscode.TextEditor) {
     this._fileName = (textEditor && textEditor.document && textEditor.document.fileName) || '';
-    this._viewColumn = (textEditor && textEditor.viewColumn) || vscode.ViewColumn.One;
   }
 
   get fileName() {
     return this._fileName;
   }
 
-  get viewColumn() {
-    return this._viewColumn;
-  }
 
   public hasSameBuffer(identity: EditorIdentity): boolean {
     return this.fileName === identity.fileName;
   }
 
   public isEqual(other: EditorIdentity): boolean {
-    return this.fileName === other.fileName && this.viewColumn === other.viewColumn;
+    return this.fileName === other.fileName;
   }
 
   public toString() {
-    return this.fileName + this.viewColumn;
+    return this.fileName;
   }
 }
