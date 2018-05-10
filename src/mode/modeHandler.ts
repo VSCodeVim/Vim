65da1e964cabd41072778a5a6578966e12997300
xconverge <sevendaystillsean@gmail.com>
2018-05-09 21:20:07 -0700

    Fix multicursor with a revert and fix gn with a hack

--- src/mode/modeHandler.ts
+++ src/mode/modeHandler.ts
@@ -796,14 +796,14 @@ export class ModeHandler implements vscode.Disposable {
       resultingCursors.push(resultingRange);
     }
 
-    // Keep track of all cursors
-    resultVimState.allCursors = resultingCursors;
-    this.vimState.editor.selections = vimState.allCursors.map(
-      cursor => new vscode.Selection(cursor.start, cursor.stop)
-    );
-
     if (vimState.recordedState.transformations.length > 0) {
       await this.executeCommand(vimState);
+    } else {
+      // Keep track of all cursors (in the case of multi-cursor).
+      resultVimState.allCursors = resultingCursors;
+      this.vimState.editor.selections = vimState.allCursors.map(
+        cursor => new vscode.Selection(cursor.start, cursor.stop)
+      );
     }
 
     return resultVimState;
