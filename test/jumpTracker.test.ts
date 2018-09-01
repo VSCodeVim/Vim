d4b711804b1c8d5ae98302677d2cbaa515bfab8c
Shawn Axsom <axs221@gmail.com>
2018-09-01 13:59:44 -0400

    More complex jumpTracker test case passed, should be good :thumbsup:

--- test/jumpTracker.test.ts
+++ test/jumpTracker.test.ts
@@ -1,16 +1,16 @@
-import * as assert from "assert";
-import * as vscode from "vscode";
+import * as assert from 'assert';
+import * as vscode from 'vscode';
 
-import { Jump } from "./../src/jumps/jump";
-import { getAndUpdateModeHandler } from "../extension";
-import { ModeHandler } from "../src/mode/modeHandler";
-import { TextEditor } from "../src/textEditor";
-import { cleanUpWorkspace, setupWorkspace } from "./testUtils";
-import { JumpTracker } from "../src/jumps/jumpTracker";
-import { Position } from "../src/common/motion/position";
-import { waitForCursorSync } from "../src/util/util";
+import { Jump } from './../src/jumps/jump';
+import { getAndUpdateModeHandler } from '../extension';
+import { ModeHandler } from '../src/mode/modeHandler';
+import { TextEditor } from '../src/textEditor';
+import { cleanUpWorkspace, setupWorkspace } from './testUtils';
+import { JumpTracker } from '../src/jumps/jumpTracker';
+import { Position } from '../src/common/motion/position';
+import { waitForCursorSync } from '../src/util/util';
 
-suite("Jump Tracker", () => {
+suite('Jump Tracker', () => {
   let jumpTracker: JumpTracker;
   let modeHandler: ModeHandler;
   let editor: vscode.TextEditor;
@@ -27,8 +27,8 @@ end`;
   const jump = (lineNumber, columnNumber) =>
     new Jump({
       editor,
-      fileName: "Untitled",
-      position: new Position(lineNumber, columnNumber)
+      fileName: 'Untitled',
+      position: new Position(lineNumber, columnNumber),
     });
 
   const start = jump(0, 0);
@@ -52,7 +52,7 @@ end`;
     await waitForCursorSync();
 
     jumpTracker = modeHandler.vimState.globalState.jumpTracker;
-    await modeHandler.handleMultipleKeyEvents(["gg"]);
+    await modeHandler.handleMultipleKeyEvents(['gg']);
     await waitForCursorSync();
     jumpTracker.clearJumps();
   });
@@ -63,12 +63,10 @@ end`;
   });
 
   const fixLineEndings = (t: string): string =>
-    process.platform === "win32" ? t.replace(/\\n/g, "\\r\\n") : t;
-  const flatten = list =>
-    list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
-  const hasPrefix = (t: string): boolean =>
-    t.startsWith("<") && t.endsWith(">");
-  const tokenize = (t: string): string[] => (hasPrefix(t) ? [t] : t.split(""));
+    process.platform === 'win32' ? t.replace(/\\n/g, '\\r\\n') : t;
+  const flatten = list => list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+  const hasPrefix = (t: string): boolean => t.startsWith('<') && t.endsWith('>');
+  const tokenize = (t: string): string[] => (hasPrefix(t) ? [t] : t.split(''));
   const sendKeys = async (keys: string[]) => {
     for (var i = 0; i < keys.length; i++) {
       const key = keys[i];
@@ -84,24 +82,30 @@ end`;
     assert.deepEqual(
       jumpTracker.jumps.map(line),
       expectedJumps.map(line),
-      "Jumps are not in the correct order or the jumps are for the wrong lines"
+      'Jumps are not in the correct order or the jumps are for the wrong lines'
     );
   };
   const assertCurrentJump = (expectedCurrentJump: Jump) =>
     assert.deepEqual(
       position(jumpTracker.currentJump || jumpTracker.end),
       position(expectedCurrentJump),
-      "Current jump is incorrect"
+      'Current jump is incorrect'
     );
 
-  const testJumps = async (
-    keys: string[],
-    jumps: Jump[],
-    currentJump: Jump
-  ) => {
-    test(`Can record jumps for key events: ${keys.map(k =>
-      k.replace("\n", "\\n")
-    )}`, async () => {
+  /**
+   * Create a test that checks both recorded jumps and final jump position.
+   *
+   * To get expected results for the tests:
+   * 1) Open vim with vim -u NONE
+   * 2) Paste in the text being tested against (text variable)
+   * 3) :clearjumps
+   * 4) Reproduce the key combinations
+   * 5) :jumps
+   * Read jumps from top to bottom, that should be in the jumps variable.
+   * The arrow (>) will be the currentJump variable.
+   */
+  const testJumps = async (keys: string[], jumps: Jump[], currentJump: Jump) => {
+    test(`Can record jumps for key events: ${keys.map(k => k.replace('\n', '\\n'))}`, async () => {
       await sendKeys(flatten(keys.map(fixLineEndings).map(tokenize)));
 
       assertJumps(jumps);
@@ -109,14 +113,20 @@ end`;
     });
   };
 
-  testJumps(["G", "gg"], [start, end], end);
-  testJumps(["G", "gg", "G"], [end, start], start);
-  testJumps(["G", "gg", "G", "gg"], [start, end], end);
-  testJumps(["/b\n", "n"], [start, b1], b1);
-  testJumps(["G", "?b\n", "gg", "G"], [end, b2, start], start);
-  testJumps(["j", "%", "%"], [open, close], close);
-  testJumps(["j", "%", "%", "<C-o>"], [close, open], close);
-  testJumps(["j", "%", "%", "<C-o>", "<C-i>"], [close, open], open);
-  testJumps(["j", "%", "%", "<C-o>", "%"], [open, close], close);
-  testJumps(["j", "%", "%", "<C-o>", "gg"], [open, close], close);
+  testJumps(['G', 'gg'], [start, end], end);
+  testJumps(['G', 'gg', 'G'], [end, start], start);
+  testJumps(['G', 'gg', 'G', 'gg'], [start, end], end);
+  testJumps(['/b\n', 'n'], [start, b1], b1);
+  testJumps(['G', '?b\n', 'gg', 'G'], [end, b2, start], start);
+  testJumps(['j', '%', '%'], [open, close], close);
+  testJumps(['j', '%', '%', '<C-o>'], [close, open], close);
+  testJumps(['j', '%', '%', '<C-o>', '<C-i>'], [close, open], open);
+  testJumps(['j', '%', '%', '<C-o>', '%'], [open, close], close);
+  testJumps(['j', '%', '%', '<C-o>', 'gg'], [open, close], close);
+  testJumps(['j', '%', '%', '<C-o>', '<C-o>', 'gg'], [open, close], close);
+  testJumps(
+    ['/^\n', 'nnn', '<C-o>', '<C-o>', '<C-o>', '<C-i>', 'gg'],
+    [start, open, b1, a2, a1],
+    a1
+  );
 });
