e1f5ddb3bf909ba22329739a146d1f620d3fb7a2
Maximiliano <maxi.padulo@gmail.com>
2018-09-10 15:05:40 +0200

    Exiting CommandMode as in Vim
    This fixes #3035, adding the missing key bindings to exit CommandMode:
    * <C-[>  which is the same as <Esc>
    * <C-c>

--- src/actions/commands/actions.ts
+++ src/actions/commands/actions.ts
@@ -1856,7 +1856,7 @@ class CommandInsertInCommandline extends BaseCommand {
 @RegisterAction
 class CommandEscInCommandline extends BaseCommand {
   modes = [ModeName.CommandlineInProgress];
-  keys = ['<Esc>'];
+  keys = [['<Esc>'], ['<C-c>'], ['<C-[>']];;
   runsOnceForEveryCursor() {
     return this.keysPressed[0] === '\n';
   }
