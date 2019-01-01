4c94ebbaabd9d0ab7c118b9183d562145b8922f8
Jason Poon <git@jasonpoon.ca>
2019-01-01 01:18:44 -0800

    fix: log warning if remapped command does not exist. closes #3307

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,6 @@
-import { configuration } from '../configuration/configuration';
 import * as winston from 'winston';
 import { ConsoleForElectron } from 'winston-console-for-electron';
+import { configuration } from '../configuration/configuration';
 
 export const logger = winston.createLogger({
   level: configuration.debug.loggingLevel,
