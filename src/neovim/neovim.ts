3183c122101e185e1a36e470afd86be7288ad0c3
xconverge <sevendaystillsean@gmail.com>
2019-01-05 16:24:58 -0800

    Change to use headless since we are not using other UI elements yet

--- src/neovim/neovim.ts
+++ src/neovim/neovim.ts
@@ -63,7 +63,7 @@ export class NeovimWrapper implements vscode.Disposable {
     if (!(await util.promisify(exists)(dir))) {
       dir = __dirname;
     }
-    this.process = spawn(configuration.neovimPath, ['-u', 'NONE', '-i', 'NONE', '-N', '--embed'], {
+    this.process = spawn(configuration.neovimPath, ['-u', 'NONE', '-n', '--embed', '--headless'], {
       cwd: dir,
     });
 
