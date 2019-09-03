2ca1cb76d65670aa9a9ac0eed045a4dae4014d6b
JoshuaRichards <8954297+JoshuaRichards@users.noreply.github.com>
2019-09-04 02:02:07 +1000

    Make `cj` linewise (#4025)
    Fixes #4024

--- src/actions/operator.ts
+++ src/actions/operator.ts
@@ -592,6 +592,7 @@ export class ChangeOperator extends BaseOperator {
 
   public async run(vimState: VimState, start: Position, end: Position): Promise<VimState> {
     const isEndOfLine = end.character === end.getLineEnd().character;
+    const isLineWise = vimState.currentRegisterMode === RegisterMode.LineWise;
     vimState = await new YankOperator(this.multicursorIndex).run(vimState, start, end);
     // which means the insert cursor would be one to the left of the end of
     // the line. We do want to run delete if it is a multiline change though ex. c}
@@ -600,7 +601,14 @@ export class ChangeOperator extends BaseOperator {
       Position.getLineLength(TextEditor.getLineAt(start).lineNumber) !== 0 ||
       end.line !== start.line
     ) {
-      if (isEndOfLine) {
+      if (isLineWise) {
+        vimState = await new DeleteOperator(this.multicursorIndex).run(
+          vimState,
+          start.getLineBegin(),
+          end.getLineEnd().getLeftThroughLineBreaks(),
+          false
+        );
+      } else if (isEndOfLine) {
         vimState = await new DeleteOperator(this.multicursorIndex).run(
           vimState,
           start,
