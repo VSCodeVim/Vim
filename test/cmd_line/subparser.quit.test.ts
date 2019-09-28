f46131d144ae827bf5daac4873e9195e98de20c9
Jason Fields <jasonfields4@gmail.com>
2019-09-27 21:34:28 -0400

    Use command abbreviations
    Vim commands are (in nearly all cases) triggered by all strings "between" the command's abbreviation and its full name.
    For instance, `:q`, `:qu`, `:qui`, and `:quit` all resolve to the same command.
    Only the full command list will autocomplete, however: `:qu<tab>` yields `:quit`.
    We now handle both these cases correctly by storing both the full name and the shortest abbreviation.

--- test/cmd_line/subparser.quit.test.ts
+++ test/cmd_line/subparser.quit.test.ts
@@ -3,39 +3,35 @@ import * as assert from 'assert';
 import { commandParsers } from '../../src/cmd_line/subparser';
 
 suite(':quit args parser', () => {
-  test('has all aliases', () => {
-    assert.equal(commandParsers.quit.name, commandParsers.q.name);
-  });
-
   test('can parse empty args', () => {
-    const args = commandParsers.quit('');
+    const args = commandParsers.quit.parser('');
     assert.equal(args.arguments.bang, undefined);
     assert.equal(args.arguments.range, undefined);
   });
 
   test('ignores trailing white space', () => {
-    const args = commandParsers.quit('  ');
+    const args = commandParsers.quit.parser('  ');
     assert.equal(args.arguments.bang, undefined);
     assert.equal(args.arguments.range, undefined);
   });
 
   test('can parse !', () => {
-    const args = commandParsers.quit('!');
+    const args = commandParsers.quit.parser('!');
     assert.ok(args.arguments.bang);
     assert.equal(args.arguments.range, undefined);
   });
 
   test('throws if space before !', () => {
-    assert.throws(() => commandParsers.quit(' !'));
+    assert.throws(() => commandParsers.quit.parser(' !'));
   });
 
   test('ignores space after !', () => {
-    const args = commandParsers.quit('! ');
+    const args = commandParsers.quit.parser('! ');
     assert.equal(args.arguments.bang, true);
     assert.equal(args.arguments.range, undefined);
   });
 
   test('throws if bad input', () => {
-    assert.throws(() => commandParsers.quit('x'));
+    assert.throws(() => commandParsers.quit.parser('x'));
   });
 });
--- test/cmd_line/subparser.substitute.test.ts
+++ test/cmd_line/subparser.substitute.test.ts
@@ -4,32 +4,32 @@ import { commandParsers } from '../../src/cmd_line/subparser';
 
 suite(':substitute args parser', () => {
   test('can parse pattern, replace, and flags', () => {
-    const args = commandParsers.s('/a/b/g');
+    const args = commandParsers.substitute.parser('/a/b/g');
     assert.equal(args.arguments.pattern, 'a');
     assert.equal(args.arguments.replace, 'b');
     assert.equal(args.arguments.flags, 8);
   });
 
   test('can parse count', () => {
-    const args = commandParsers.s('/a/b/g 3');
+    const args = commandParsers.substitute.parser('/a/b/g 3');
     assert.equal(args.arguments.count, 3);
   });
 
   test('can parse custom delimiter', () => {
-    const args = commandParsers.s('#a#b#g');
+    const args = commandParsers.substitute.parser('#a#b#g');
     assert.equal(args.arguments.pattern, 'a');
     assert.equal(args.arguments.replace, 'b');
     assert.equal(args.arguments.flags, 8);
   });
 
   test('can escape delimiter', () => {
-    const args = commandParsers.s('/\\/\\/a/b/');
+    const args = commandParsers.substitute.parser('/\\/\\/a/b/');
     assert.equal(args.arguments.pattern, '//a');
     assert.equal(args.arguments.replace, 'b');
   });
 
   test('can parse flag KeepPreviousFlags', () => {
-    const args = commandParsers.s('/a/b/&');
+    const args = commandParsers.substitute.parser('/a/b/&');
     assert.equal(args.arguments.flags, 1);
   });
 });
