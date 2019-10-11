bb4aca2b66f22b795f89691cdbf7bc5bd943d486
xconverge <sevendaystillsean@gmail.com>
2019-10-10 19:22:54 -0700

    Try to parse neovim from PATH if neovimPath is empty

--- src/configuration/validators/neovimValidator.ts
+++ src/configuration/validators/neovimValidator.ts
@@ -8,12 +8,31 @@ export class NeovimValidator implements IConfigurationValidator {
     const result = new ValidatorResults();
 
     if (config.enableNeovim) {
+      let triedToParsePath = false;
       try {
+        // Try to find nvim in path if it is not defined
+        if (config.neovimPath === '') {
+          const pathVar = process.env.PATH;
+          if (pathVar) {
+            pathVar.split(';').forEach(element => {
+              if (element.toLocaleLowerCase().includes('neovim')) {
+                const path = require('path');
+                config.neovimPath = element + path.sep + 'nvim';
+                triedToParsePath = true;
+                return;
+              }
+            });
+          }
+        }
         await promisify(execFile)(config.neovimPath, ['--version']);
       } catch (e) {
+        let errorMessage = `Invalid neovimPath. ${e.message}.`;
+        if (triedToParsePath) {
+          errorMessage += 'Tried to parse PATH.';
+        }
         result.append({
           level: 'error',
-          message: `Invalid neovimPath. ${e.message}.`,
+          message: errorMessage,
         });
       }
     }
