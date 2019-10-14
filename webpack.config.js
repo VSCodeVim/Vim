da859096b6ef2ef95a64c450f62b340c5dd0b9f0
Jason Poon <jason.the.poon@oracle.com>
2019-10-14 01:02:41 -0700

    Revert "fix: exclude neovim from webpack module. #4126 (#4169)"
    This reverts commit 200c17f3df5bca193b3b9badeefdb9bbf27ca827.

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ const config = {
   devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    neovim: 'neovim',
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
