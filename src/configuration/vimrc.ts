35e11ade5e30f5c5dbe4c8d70375ab78c4780df5
Jason Fields <jasonfields4@gmail.com>
2019-12-27 01:17:26 -0500

    Offer to create .vimrc if it doesn't already exist
    Also fix a few other little things with our .vimrc support
    Fixes #4325

--- src/configuration/vimrc.ts
+++ src/configuration/vimrc.ts
@@ -1,8 +1,11 @@
 import * as _ from 'lodash';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { IConfiguration, IVimrcKeyRemapping } from './iconfiguration';
 import { vimrcKeyRemappingBuilder } from './vimrcKeyRemappingBuilder';
+import { window, workspace } from 'vscode';
+import { configuration } from './configuration';
 
 class VimrcImpl {
   private _vimrcPath: string;
@@ -18,22 +21,40 @@ class VimrcImpl {
     const _path = config.vimrc.path
       ? VimrcImpl.expandHome(config.vimrc.path)
       : VimrcImpl.findDefaultVimrc();
-    if (!_path || !fs.existsSync(_path)) {
-      // TODO: we may want to offer to create the file for them
-      // throw new Error(`Unable to find .vimrc file`);
-      return; // it is okay to fail when there is no .vimrc
+    if (!_path) {
+      await window.showWarningMessage('No .vimrc found. Please set `vim.vimrc.path.`');
+      return;
     }
-    this._vimrcPath = _path;
+    if (!fs.existsSync(_path)) {
+      window
+        .showWarningMessage(`No .vimrc found at ${_path}.`, 'Create it')
+        .then(async (choice: string | undefined) => {
+          if (choice === 'Create it') {
+            const newVimrc = await vscode.window.showSaveDialog({
+              defaultUri: vscode.Uri.file(_path),
+            });
+            if (newVimrc) {
+              fs.writeFileSync(newVimrc.fsPath, '');
+              configuration.getConfiguration('vim').update('vimrc.path', newVimrc.fsPath, true);
+              await vscode.workspace.openTextDocument(newVimrc);
+              // TODO: add some sample remaps/settings in here?
+              await vscode.window.showTextDocument(newVimrc);
+            }
+          }
+        });
+    } else {
+      this._vimrcPath = _path;
 
-    // Remove all the old remappings from the .vimrc file
-    VimrcImpl.removeAllRemapsFromConfig(config);
+      // Remove all the old remappings from the .vimrc file
+      VimrcImpl.removeAllRemapsFromConfig(config);
 
-    // Add the new remappings
-    const lines = fs.readFileSync(this.vimrcPath, { encoding: 'utf8' }).split(/\r?\n/);
-    for (const line of lines) {
-      const remap = await vimrcKeyRemappingBuilder.build(line);
-      if (remap) {
-        VimrcImpl.addRemapToConfig(config, remap);
+      // Add the new remappings
+      const lines = fs.readFileSync(this.vimrcPath, { encoding: 'utf8' }).split(/\r?\n/);
+      for (const line of lines) {
+        const remap = await vimrcKeyRemappingBuilder.build(line);
+        if (remap) {
+          VimrcImpl.addRemapToConfig(config, remap);
+        }
       }
     }
   }
@@ -68,7 +89,7 @@ class VimrcImpl {
         case 'cnoremap':
           return [config.commandLineModeKeyBindingsNonRecursive];
         default:
-          console.warn('Encountered an unrecognized mapping type.');
+          console.warn(`Encountered an unrecognized mapping type: '${remap.keyRemappingType}'`);
           return undefined;
       }
     })();
@@ -86,9 +107,11 @@ class VimrcImpl {
       config.normalModeKeyBindings,
       config.visualModeKeyBindings,
       config.insertModeKeyBindings,
+      config.commandLineModeKeyBindings,
       config.normalModeKeyBindingsNonRecursive,
       config.visualModeKeyBindingsNonRecursive,
       config.insertModeKeyBindingsNonRecursive,
+      config.commandLineModeKeyBindingsNonRecursive,
     ];
     for (const remaps of remapCollections) {
       _.remove(remaps, remap => remap.source === 'vimrc');
