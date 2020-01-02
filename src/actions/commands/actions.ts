f92a8818d4232872f8ff1ea8a9a475ce13443922
CVVPK <cvaldizon93@gmail.com>
2020-01-01 19:24:19 -0500

    <copy> and <C-O> may change document

--- src/actions/commands/actions.ts
+++ src/actions/commands/actions.ts
@@ -1123,6 +1123,11 @@ class CommandPasteInSearchMode extends BaseCommand {
 class CommandOverrideCopy extends BaseCommand {
   modes = [Mode.Visual, Mode.VisualLine, Mode.VisualBlock, Mode.Insert, Mode.Normal];
   keys = ['<copy>']; // A special key - see ModeHandler
+
+  // We need this because <C-c>(CommandEscInsertMode)
+  // may be overwritten to <copy>
+  mightChangeDocument = true;
+
   runsOnceForEveryCursor() {
     return false;
   }
