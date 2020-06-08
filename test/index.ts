68f89cda4e0925ca834a25dd0f3a3200538afe2f
Jason Fields <jasonfields4@gmail.com>
2020-06-08 11:02:05 -0400

    Fix warning about Mocha's `useColors` being deprecated

--- test/index.ts
+++ test/index.ts
@@ -25,11 +25,10 @@ export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
-    useColors: true,
+    color: true,
     timeout: 10000,
     grep: mochaGrep,
   });
-  mocha.useColors(true);
 
   const testsRoot = path.resolve(__dirname, '..');
 
