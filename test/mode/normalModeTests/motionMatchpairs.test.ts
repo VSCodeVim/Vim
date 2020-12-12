54e495ff0eb03bfced16f77022069be8baf7feaa
sql-koala <sql-koala@github>
2020-12-12 13:53:07 +0100

    direct import newTest, fixed.

--- test/mode/normalModeTests/motionMatchpairs.test.ts
+++ test/mode/normalModeTests/motionMatchpairs.test.ts
@@ -1,4 +1,4 @@
-import { newTest } from '../testSimplifier';
+import { newTest } from '../../testSimplifier';
 import { cleanUpWorkspace, setupWorkspace } from './../../testUtils';
 import { Configuration } from '../../testConfiguration';
 
