c3e04ef5477ce340437356141dcc834919d8f90e
Max Nordlund <max.nordlund@gmail.com>
2021-03-11 02:38:42 +0100

    Add Erlang style comments (#5970)

--- src/actions/operator.ts
+++ src/actions/operator.ts
@@ -885,6 +885,7 @@ class ActionVisualReflowParagraph extends BaseOperator {
     { singleLine: true, start: '#' },
     { singleLine: true, start: ';' },
     { singleLine: true, start: '*' },
+    { singleLine: true, start: '%' },
 
     // Needs to come last, since everything starts with the empty string!
     { singleLine: true, start: '' },
