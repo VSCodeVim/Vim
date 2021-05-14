aacad5167e78f110e6375b69a36111786a63ceec
Jason Fields <jasonfields4@gmail.com>
2021-05-14 00:55:22 -0400

    Fix logic for merging content changes that came from a tab completion
    This makes repeating an insertion that contains a tab completion work again (this broke when we moved to using transformations). As a bonus, it's all done in a single transformation. Yay!
    The core logic here is essentially identical to that in HistoryTracker, and it should probably be abstracted out and unit tested.
    This deserves integration testing as well, but that'll require a bit of nuance, and it's getting late.
    Fixes #6610

--- src/actions/commands/actions.ts
+++ src/actions/commands/actions.ts
@@ -37,6 +37,14 @@ import { WriteQuitCommand } from '../../cmd_line/commands/writequit';
 import { shouldWrapKey } from '../wrapping';
 import { ErrorCode, VimError } from '../../error';
 
+/**
+ * A very special snowflake.
+ *
+ * Each keystroke when typing in Insert mode is its own Action, which means naively replaying a
+ * realistic insertion (via `.` or a macro) does many small insertions, which is very slow.
+ * So instead, we fold all those actions after the fact into a single DocumentContentChangeAction,
+ * which compresses the changes, generally into a single document edit per cursor.
+ */
 export class DocumentContentChangeAction extends BaseCommand {
   modes: [];
 
@@ -128,19 +136,35 @@ export class DocumentContentChangeAction extends BaseCommand {
       second: vscode.TextDocumentContentChangeEvent
     ): vscode.TextDocumentContentChangeEvent | undefined {
       if (
-        first.rangeOffset + first.text.length !== second.rangeOffset ||
-        second.rangeLength !== 0
+        first.rangeOffset + first.text.length === second.rangeOffset ||
+        second.rangeLength === 0
       ) {
-        // TODO: We should be able to do better, but I'm not sure if this is actually relevant.
+        // Simple concatenation
+        return {
+          text: first.text + second.text,
+          range: first.range,
+          rangeOffset: first.rangeOffset,
+          rangeLength: first.rangeLength,
+        };
+      } else if (
+        first.rangeOffset <= second.rangeOffset &&
+        first.text.length >= second.rangeLength
+      ) {
+        const start = second.rangeOffset - first.rangeOffset;
+        const end = start + second.rangeLength;
+        const text = first.text.slice(0, start) + second.text + first.text.slice(end);
+        // `second` replaces part of `first`
+        // Most often, this is the result of confirming an auto-completion
+        return {
+          text,
+          range: first.range,
+          rangeOffset: first.rangeOffset,
+          rangeLength: first.rangeLength,
+        };
+      } else {
+        // TODO: Do any of the cases falling into this `else` matter?
         return undefined;
       }
-
-      return {
-        text: first.text + second.text,
-        range: first.range,
-        rangeOffset: first.rangeOffset,
-        rangeLength: first.rangeLength,
-      };
     }
 
     const compressed: vscode.TextDocumentContentChangeEvent[] = [];
