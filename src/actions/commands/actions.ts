VSCodeVim/Vim

commit 9091da09cc1c0a37bf3e659e11a7f2f9e0ea4999

Make `VimState.isMultiCursor` a function
I can't see any reason why this would be incorrect, and if it does break something, that something is wrong and should be fixed - there's no reason to maintain this information manually.

src/actions/commands/actions.ts
@@ -436,8 +436,6 @@ class CommandEsc extends BaseCommand {
         ]);
 
         return;
-      } else {
-        vimState.isMultiCursor = false;
       }
     }
 
@@ -1615,7 +1613,6 @@ class CommandInsertNewLineAbove extends BaseCommand {
     }
     vimState.cursors = vimState.cursors.reverse();
     vimState.isFakeMultiCursor = true;
-    vimState.isMultiCursor = true;
   }
 }
 
@@ -1654,7 +1651,6 @@ class CommandInsertNewLineBefore extends BaseCommand {
     }
     vimState.cursors = vimState.cursors.reverse();
     vimState.isFakeMultiCursor = true;
-    vimState.isMultiCursor = true;
   }
 }
 
@@ -2489,7 +2485,6 @@ class ActionGoToInsertVisualBlockMode extends BaseCommand {
 
   public async exec(position: Position, vimState: VimState): Promise<void> {
     await vimState.setCurrentMode(Mode.Insert);
-    vimState.isMultiCursor = true;
     vimState.isFakeMultiCursor = true;
 
     for (const { line, start } of TextEditor.iterateLinesInBlock(vimState)) {
@@ -2520,7 +2515,6 @@ class ActionChangeInVisualBlockMode extends BaseCommand {
     }
 
     await vimState.setCurrentMode(Mode.Insert);
-    vimState.isMultiCursor = true;
     vimState.isFakeMultiCursor = true;
 
     for (const { start } of TextEditor.iterateLinesInBlock(vimState)) {
@@ -2553,7 +2547,6 @@ class ActionChangeToEOLInVisualBlockMode extends BaseCommand {
     vimState.cursors = cursors;
 
     await vimState.setCurrentMode(Mode.Insert);
-    vimState.isMultiCursor = true;
     vimState.isFakeMultiCursor = true;
   }
 }
@@ -2571,7 +2564,6 @@ abstract class ActionGoToInsertVisualLineModeCommand extends BaseCommand {
 
   public async exec(position: Position, vimState: VimState): Promise<void> {
     await vimState.setCurrentMode(Mode.Insert);
-    vimState.isMultiCursor = true;
     vimState.isFakeMultiCursor = true;
 
     const resultingCursors: Cursor[] = [];
@@ -2693,7 +2685,6 @@ class ActionGoToInsertVisualBlockModeAppend extends BaseCommand {
 
     vimState.cursors = newCursors;
     await vimState.setCurrentMode(Mode.Insert);
-    vimState.isMultiCursor = true;
     vimState.isFakeMultiCursor = true;
   }
 }

src/actions/commands/insert.ts
@@ -106,7 +106,6 @@ export class CommandEscInsertMode extends BaseCommand {
 
     if (vimState.isFakeMultiCursor) {
       vimState.cursors = [vimState.cursors[0]];
-      vimState.isMultiCursor = false;
       vimState.isFakeMultiCursor = false;
     }
   }

src/mode/modeHandler.ts
@@ -288,10 +288,6 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
       return;
     }
 
-    if (e.selections.length === 1) {
-      this.vimState.isMultiCursor = false;
-    }
-
     if (isStatusBarMode(this.vimState.currentMode)) {
       return;
     }

src/state/vimState.ts
@@ -71,8 +71,9 @@ export class VimState implements vscode.Disposable {
   /**
    * Are multiple cursors currently present?
    */
-  // TODO: why isn't this a function?
-  public isMultiCursor = false;
+  public get isMultiCursor(): boolean {
+    return this._cursors.length > 1;
+  }
 
   /**
    * Is the multicursor something like visual block "multicursor", where
@@ -181,7 +182,6 @@ export class VimState implements vscode.Disposable {
     }
 
     this._cursors = [...map.values()];
-    this.isMultiCursor = this._cursors.length > 1;
   }
 
   /**

commit fce459634c32969c2bd1f3923f073c8e354d0a76

`<Esc>` should cancel multi-cursor
This regression was caused by 9091da09cc1c0a37bf3e659e11a7f2f9e0ea4999
Fixes #6719

src/actions/commands/actions.ts
@@ -447,7 +447,7 @@ class CommandEsc extends BaseCommand {
 
     await vimState.setCurrentMode(Mode.Normal);
 
-    if (!vimState.isMultiCursor) {
+    if (vimState.isMultiCursor) {
       vimState.cursors = [vimState.cursors[0]];
     }
   }