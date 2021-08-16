8293a6c176ee0320120951fb4dfca44eb05b5bef
Devin Leamy <devinleamy@gmail.com>
2021-08-15 20:58:36 -0600

    adjusted parser to match vim's behaviour

--- src/cmd_line/subparsers/deleteRange.ts
+++ src/cmd_line/subparsers/deleteRange.ts
@@ -7,14 +7,26 @@ export function parseDeleteRangeLinesCommandArgs(args: string): node.DeleteRange
   }
 
   /**
-   * Note that to specify a register, a [cnt] for :d[elete][cnt]
-   * must be specified.
-   * Ex: :d4 i -> specifies register i
-   *     :d  i -> does not specify a register
+   * :d[elete] [register] [cnt]
+   * Note: the first argument is a [register], unless it's a number, in which case
+   * it's the [cnt]
    */
   const scanner = new Scanner(args);
+  const arg1 = scanner.nextWord(); // [cnt] or [register]
+  const arg2 = scanner.nextWord(); // [cnt] or EOF
+
+  let register;
+  let linesToRemove;
+
+  if (isNaN(+arg1)) {
+    register = arg1;
+    linesToRemove = isNaN(+arg2) ? 1 : +arg2;
+  } else {
+    linesToRemove = +arg1;
+  }
+
   return new node.DeleteRangeCommand({
-    linesToRemove: +scanner.nextWord() ?? 1,
-    register: scanner.isAtEof ? undefined : scanner.nextWord(),
+    register,
+    linesToRemove,
   });
 }
