1424ce6b2f747d4c090933ea422e23f67cfeaced
Jason Fields <jasonfields4@gmail.com>
2021-11-01 01:58:34 -0400

    Slight refactor combining a few actions

--- src/actions/commands/search.ts
+++ src/actions/commands/search.ts
@@ -6,7 +6,7 @@ import { SearchCommandLine } from '../../cmd_line/commandLine';
 import { sorted } from '../../common/motion/position';
 import { configuration } from '../../configuration/configuration';
 import { VimError, ErrorCode } from '../../error';
-import { Mode } from '../../mode/mode';
+import { isVisualMode, Mode } from '../../mode/mode';
 import { Register } from '../../register/register';
 import { globalState } from '../../state/globalState';
 import { SearchState } from '../../state/searchState';
@@ -142,14 +142,18 @@ async function createSearchStateAndMoveToMatch(args: {
 
 @RegisterAction
 class CommandSearchCurrentWordExactForward extends BaseCommand {
-  modes = [Mode.Normal];
+  modes = [Mode.Normal, Mode.Visual, Mode.VisualLine];
   keys = ['*'];
   override isMotion = true;
   override runsOnceForEachCountPrefix = true;
   override isJump = true;
 
   public override async exec(position: Position, vimState: VimState): Promise<void> {
-    await searchCurrentWord(position, vimState, SearchDirection.Forward, true);
+    if (isVisualMode(vimState.currentMode) && configuration.visualstar) {
+      await searchCurrentSelection(vimState, SearchDirection.Forward);
+    } else {
+      await searchCurrentWord(position, vimState, SearchDirection.Forward, true);
+    }
   }
 }
 
@@ -167,32 +171,19 @@ class CommandSearchCurrentWordForward extends BaseCommand {
 }
 
 @RegisterAction
-class CommandSearchVisualForward extends BaseCommand {
-  modes = [Mode.Visual, Mode.VisualLine];
-  keys = ['*'];
-  override isMotion = true;
-  override runsOnceForEachCountPrefix = true;
-  override isJump = true;
-
-  public override async exec(position: Position, vimState: VimState): Promise<void> {
-    if (configuration.visualstar) {
-      await searchCurrentSelection(vimState, SearchDirection.Forward);
-    } else {
-      await searchCurrentWord(position, vimState, SearchDirection.Forward, true);
-    }
-  }
-}
-
-@RegisterAction
 class CommandSearchCurrentWordExactBackward extends BaseCommand {
-  modes = [Mode.Normal];
+  modes = [Mode.Normal, Mode.Visual, Mode.VisualLine];
   keys = ['#'];
   override isMotion = true;
   override runsOnceForEachCountPrefix = true;
   override isJump = true;
 
   public override async exec(position: Position, vimState: VimState): Promise<void> {
-    await searchCurrentWord(position, vimState, SearchDirection.Backward, true);
+    if (configuration.visualstar) {
+      await searchCurrentSelection(vimState, SearchDirection.Backward);
+    } else {
+      await searchCurrentWord(position, vimState, SearchDirection.Backward, true);
+    }
   }
 }
 
@@ -206,23 +197,6 @@ class CommandSearchCurrentWordBackward extends BaseCommand {
 
   public override async exec(position: Position, vimState: VimState): Promise<void> {
     await searchCurrentWord(position, vimState, SearchDirection.Backward, false);
-  }
-}
-
-@RegisterAction
-class CommandSearchVisualBackward extends BaseCommand {
-  modes = [Mode.Visual, Mode.VisualLine];
-  keys = ['#'];
-  override isMotion = true;
-  override runsOnceForEachCountPrefix = true;
-  override isJump = true;
-
-  public override async exec(position: Position, vimState: VimState): Promise<void> {
-    if (configuration.visualstar) {
-      await searchCurrentSelection(vimState, SearchDirection.Backward);
-    } else {
-      await searchCurrentWord(position, vimState, SearchDirection.Backward, true);
-    }
   }
 }
 
