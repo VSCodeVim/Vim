0463f53929169afaacc83f210a352c2c422881e7
waynewaynetsai <wayne040904007@gmail.com>
2022-01-06 10:43:02 +0800

    style: rename first <copy> to <Esc> at test case for readability

--- test/mode/modeInsert.test.ts
+++ test/mode/modeInsert.test.ts
@@ -57,8 +57,8 @@ suite('Mode Insert', () => {
     return assertEqualLines(['text', '']);
   });
 
-  test('<copy> should not override system-clipboard after exiting insert mode', async () => {
-    const yankTextAtSystemClipboard = ['i', 't', 'e', 'x', 't', '<copy>', 'v', 'i', 'w', '<copy>'];
+  test.only('<copy> should not override system-clipboard after exiting insert mode', async () => {
+    const yankTextAtSystemClipboard = ['i', 't', 'e', 'x', 't', '<Esc>', 'v', 'i', 'w', '<copy>'];
 
     const pasteTextAtInsertMode = ['a', '<C-r>', '+', '<copy>'];
 
