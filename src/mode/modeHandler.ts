VSCodeVim/Vim

commit a4656a6e53a81e1ae4e2406f81ad831afffd0a18

Move `VimState.commandLine` into `modeData`

src/mode/modeHandler.ts
@@ -24,7 +24,6 @@ import { TextEditor } from './../textEditor';
 import { VimError, ForceStopRemappingError } from './../error';
 import { VimState } from './../state/vimState';
 import { VSCodeContext } from '../util/vscodeContext';
-import { SearchCommandLine } from '../cmd_line/commandLine';
 import { configuration } from '../configuration/configuration';
 import { decoration } from '../configuration/decoration';
 import { scrollView } from '../util/util';
@@ -75,9 +74,11 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
     return this._currentMode;
   }
 
-  private async setCurrentMode(modeName: Mode): Promise<void> {
-    await this.vimState.setCurrentMode(modeName);
-    this._currentMode = modeName;
+  private async setCurrentMode(mode: Mode): Promise<void> {
+    if (this.vimState.currentMode !== mode) {
+      await this.vimState.setCurrentMode(mode);
+    }
+    this._currentMode = mode;
   }
 
   public static async create(
@@ -1129,13 +1130,16 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
   }
 
   public updateSearchHighlights(showHighlights: boolean) {
+    const modeData = this.vimState.modeData;
     const {
       searchHighlight = [],
       searchMatch = [],
       substitutionAppend = [],
       substitutionReplace = [],
-    }: SearchDecorations = showHighlights
-      ? this.vimState.commandLine?.getDecorations(this.vimState) ??
+    }: SearchDecorations = (modeData.mode === Mode.CommandlineInProgress ||
+      modeData.mode === Mode.SearchInProgressMode) &&
+    showHighlights
+      ? modeData.commandLine.getDecorations(this.vimState) ??
         // if there are no decorations from the command line, get decorations for previous search state
         getDecorationsForSearchMatchRanges(globalState.searchState?.getMatchRanges(this.vimState))
       : {};
@@ -1155,13 +1159,11 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
     // Draw selection (or cursor)
     if (args.drawSelection) {
       let selectionMode: Mode = this.vimState.currentMode;
-      if (this.vimState.currentMode === Mode.SearchInProgressMode) {
-        // TODO: Alleviate need for this type assertion
-        selectionMode = this.vimState.commandLine!.previousMode;
-      } else if (this.vimState.currentMode === Mode.CommandlineInProgress) {
-        // TODO: Alleviate need for this type assertion
-        selectionMode = this.vimState.commandLine!.previousMode;
-      } else if (this.vimState.currentMode === Mode.SurroundInputMode) {
+      if (this.vimState.modeData.mode === Mode.SearchInProgressMode) {
+        selectionMode = this.vimState.modeData.commandLine.previousMode;
+      } else if (this.vimState.modeData.mode === Mode.CommandlineInProgress) {
+        selectionMode = this.vimState.modeData.commandLine.previousMode;
+      } else if (this.vimState.modeData.mode === Mode.SurroundInputMode) {
         selectionMode = this.vimState.surround!.previousMode;
       }
 
@@ -1342,12 +1344,8 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
         ? vscode.TextEditorRevealType.InCenter
         : vscode.TextEditorRevealType.Default;
 
-      if (
-        this.vimState.currentMode === Mode.SearchInProgressMode &&
-        this.vimState.commandLine instanceof SearchCommandLine &&
-        configuration.incsearch
-      ) {
-        const currentMatch = this.vimState.commandLine.getCurrentMatchRange(this.vimState);
+      if (this.vimState.modeData.mode === Mode.SearchInProgressMode && configuration.incsearch) {
+        const currentMatch = this.vimState.modeData.commandLine.getCurrentMatchRange(this.vimState);
 
         if (currentMatch) {
           this.vimState.editor.revealRange(currentMatch.range, revealType);

src/actions/commands/actions.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 
 import { RecordedState } from '../../state/recordedState';
-import { ReplaceState } from '../../state/replaceState';
 import { VimState } from '../../state/vimState';
 import { getCursorsAfterSync, clamp } from '../../util/util';
 import { Clipboard } from '../../util/clipboard';
@@ -770,8 +769,12 @@ class CommandShowCommandLine extends BaseCommand {
       commandLineText = "'<,'>";
     }
 
-    vimState.commandLine = new ExCommandLine(commandLineText, vimState.currentMode);
+    const previousMode = vimState.currentMode;
     await vimState.setCurrentMode(Mode.CommandlineInProgress);
+    // TODO: Change or supplement `setCurrentMode` API so this isn't necessary
+    if (vimState.modeData.mode === Mode.CommandlineInProgress) {
+      vimState.modeData.commandLine = new ExCommandLine(commandLineText, previousMode);
+    }
   }
 }
 

src/actions/commands/commandLine.ts
@@ -11,7 +11,6 @@ import { StatusBar } from '../../statusBar';
 import { getPathDetails, readDirectory } from '../../util/path';
 import { Clipboard } from '../../util/clipboard';
 import { VimError, ErrorCode } from '../../error';
-import { assertDefined } from '../../util/util';
 import { builtinExCommands } from '../../vimscript/exCommandParser';
 import { SearchDirection } from '../../vimscript/pattern';
 
@@ -25,9 +24,16 @@ abstract class CommandLineAction extends BaseCommand {
   protected abstract run(vimState: VimState, commandLine: CommandLine): Promise<void>;
 
   public override async exec(position: vscode.Position, vimState: VimState): Promise<void> {
-    assertDefined<CommandLine>(vimState.commandLine, 'vimState.commandLine unexpectedly undefined');
+    if (
+      !(
+        vimState.modeData.mode === Mode.CommandlineInProgress ||
+        vimState.modeData.mode === Mode.SearchInProgressMode
+      )
+    ) {
+      throw new Error(`Unexpected mode ${vimState.modeData.mode} in CommandLineAction`);
+    }
 
-    await this.run(vimState, vimState.commandLine);
+    await this.run(vimState, vimState.modeData.commandLine);
   }
 }
 
@@ -146,6 +152,7 @@ class ExCommandLineEnter extends CommandLineAction {
 
   protected override async run(vimState: VimState, commandLine: CommandLine): Promise<void> {
     await commandLine.run(vimState);
+    await vimState.setCurrentMode(Mode.Normal);
   }
 }
 
@@ -161,6 +168,10 @@ class SearchCommandLineEnter extends CommandLineAction {
 
   protected override async run(vimState: VimState, commandLine: CommandLine): Promise<void> {
     await commandLine.run(vimState);
+    if (this.multicursorIndex === vimState.cursors.length - 1) {
+      // TODO: gah, this is stupid
+      await vimState.setCurrentMode(commandLine.previousMode);
+    }
   }
 }
 

src/actions/commands/search.ts
@@ -210,10 +210,7 @@ class CommandSearchForwards extends BaseCommand {
   }
 
   public override async exec(position: Position, vimState: VimState): Promise<void> {
-    vimState.commandLine = new SearchCommandLine(vimState, '', SearchDirection.Forward);
     await vimState.setCurrentMode(Mode.SearchInProgressMode);
-
-    globalState.searchState = vimState.commandLine.getSearchState();
   }
 }
 
@@ -228,10 +225,12 @@ class CommandSearchBackwards extends BaseCommand {
   }
 
   public override async exec(position: Position, vimState: VimState): Promise<void> {
-    vimState.commandLine = new SearchCommandLine(vimState, '', SearchDirection.Backward);
-    await vimState.setCurrentMode(Mode.SearchInProgressMode);
-
-    globalState.searchState = vimState.commandLine.getSearchState();
+    // TODO: Better VimState API than this...
+    await vimState.setModeData({
+      mode: Mode.SearchInProgressMode,
+      commandLine: new SearchCommandLine(vimState, '', SearchDirection.Backward),
+      firstVisibleLineBeforeSearch: vimState.editor.visibleRanges[0].start.line,
+    });
   }
 }
 

src/actions/operator.ts
@@ -248,8 +248,12 @@ class FilterOperator extends BaseOperator {
       vimState.cursors = vimState.cursorsInitialState;
     }
 
-    vimState.commandLine = new ExCommandLine(commandLineText, vimState.currentMode);
+    const previousMode = vimState.currentMode;
     await vimState.setCurrentMode(Mode.CommandlineInProgress);
+    // TODO: Change or supplement `setCurrentMode` API so this isn't necessary
+    if (vimState.modeData.mode === Mode.CommandlineInProgress) {
+      vimState.modeData.commandLine = new ExCommandLine(commandLineText, previousMode);
+    }
   }
 }
 

src/cmd_line/commandLine.ts
@@ -13,12 +13,8 @@ import { getWordLeftInText, getWordRightInText, WordType } from '../textobject/w
 import { CommandShowCommandHistory, CommandShowSearchHistory } from '../actions/commands/actions';
 import { SearchDirection } from '../vimscript/pattern';
 import { reportSearch, escapeCSSIcons } from '../util/statusBarTextUtils';
-import {
-  SearchDecorations,
-  ensureVisible,
-  getDecorationsForSearchMatchRanges,
-} from '../util/decorationUtils';
-import { Position, ExtensionContext, window, DecorationOptions, Range } from 'vscode';
+import { SearchDecorations, getDecorationsForSearchMatchRanges } from '../util/decorationUtils';
+import { Position, ExtensionContext, window } from 'vscode';
 import { globalState } from '../state/globalState';
 import { scrollView } from '../util/util';
 import { ExCommand } from '../vimscript/exCommand';
@@ -307,8 +303,6 @@ export class ExCommandLine extends CommandLine {
 
     // Update state if this command is repeatable via dot command.
     vimState.lastCommandDotRepeatable = this.command?.isRepeatableWithDot ?? false;
-
-    await vimState.setCurrentMode(Mode.Normal);
   }
 
   public async escape(vimState: VimState): Promise<void> {
@@ -468,8 +462,6 @@ export class SearchCommandLine extends CommandLine {
   }
 
   public async run(vimState: VimState): Promise<void> {
-    await vimState.setCurrentMode(this.previousMode);
-
     // Repeat the previous search if no new string is entered
     if (this.text === '') {
       if (SearchCommandLine.previousSearchStates.length > 0) {

src/mode/mode.ts
@@ -38,7 +38,9 @@ export function isVisualMode(mode: Mode): mode is Mode.Visual | Mode.VisualLine 
  * Is the given mode one where the cursor is on the status bar?
  * This means SearchInProgess and CommandlineInProgress modes.
  */
-export function isStatusBarMode(mode: Mode): boolean {
+export function isStatusBarMode(
+  mode: Mode
+): mode is Mode.CommandlineInProgress | Mode.SearchInProgressMode {
   return [Mode.SearchInProgressMode, Mode.CommandlineInProgress].includes(mode);
 }
 

src/mode/modeData.ts
@@ -1,8 +1,12 @@
+import { ExCommandLine, SearchCommandLine } from '../cmd_line/commandLine';
 import { ReplaceState } from '../state/replaceState';
 import { Mode } from './mode';
 
 /** Modes which have no extra associated data. */
-export type SimpleMode = Exclude<Mode, Mode.Replace | Mode.SearchInProgressMode>;
+export type SimpleMode = Exclude<
+  Mode,
+  Mode.Replace | Mode.SearchInProgressMode | Mode.CommandlineInProgress
+>;
 
 /** State associated with the current mode. */
 export type ModeData =
@@ -11,7 +15,12 @@ export type ModeData =
       replaceState: ReplaceState;
     }
   | {
+      mode: Mode.CommandlineInProgress;
+      commandLine: ExCommandLine;
+    }
+  | {
       mode: Mode.SearchInProgressMode;
+      commandLine: SearchCommandLine;
       /** The first line number that was visible when SearchInProgressMode began */
       firstVisibleLineBeforeSearch: number;
     }

commit 712674b7e08ef56ddad73b4a5b6a6d64dee6a02d

Fix search highlights
This was broken by a4656a6e53a81e1ae4e2406f81ad831afffd0a18

src/mode/modeHandler.ts
@@ -1155,24 +1155,34 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
   }
 
   public updateSearchHighlights(showHighlights: boolean) {
-    const modeData = this.vimState.modeData;
-    const {
-      searchHighlight = [],
-      searchMatch = [],
-      substitutionAppend = [],
-      substitutionReplace = [],
-    }: SearchDecorations = (modeData.mode === Mode.CommandlineInProgress ||
-      modeData.mode === Mode.SearchInProgressMode) &&
-    showHighlights
-      ? modeData.commandLine.getDecorations(this.vimState) ??
-        // if there are no decorations from the command line, get decorations for previous search state
-        getDecorationsForSearchMatchRanges(globalState.searchState?.getMatchRanges(this.vimState))
-      : {};
+    let decorations: SearchDecorations | undefined;
+    if (showHighlights) {
+      if (
+        this.vimState.modeData.mode === Mode.CommandlineInProgress ||
+        this.vimState.modeData.mode === Mode.SearchInProgressMode
+      ) {
+        decorations = this.vimState.modeData.commandLine.getDecorations(this.vimState);
+      } else {
+        // If there are no decorations from the command line, get decorations for previous SearchState
+        decorations = getDecorationsForSearchMatchRanges(
+          globalState.searchState?.getMatchRanges(this.vimState)
+        );
+      }
+    }
 
-    this.vimState.editor.setDecorations(decoration.searchHighlight, searchHighlight);
-    this.vimState.editor.setDecorations(decoration.searchMatch, searchMatch);
-    this.vimState.editor.setDecorations(decoration.substitutionAppend, substitutionAppend);
-    this.vimState.editor.setDecorations(decoration.substitutionReplace, substitutionReplace);
+    this.vimState.editor.setDecorations(
+      decoration.searchHighlight,
+      decorations?.searchHighlight ?? []
+    );
+    this.vimState.editor.setDecorations(decoration.searchMatch, decorations?.searchMatch ?? []);
+    this.vimState.editor.setDecorations(
+      decoration.substitutionAppend,
+      decorations?.substitutionAppend ?? []
+    );
+    this.vimState.editor.setDecorations(
+      decoration.substitutionReplace,
+      decorations?.substitutionReplace ?? []
+    );
   }
 
   public async updateView(