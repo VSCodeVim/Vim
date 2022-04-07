d9ce88c12cebdde6fc912888d49b3cc4b370b1af
Jason Fields <jasonfields4@gmail.com>
2022-04-06 23:41:21 -0400

    Fix a few bugs in `:read`
    - Defaults to current file
    - Inserts below current line, not at current position

--- src/cmd_line/commands/read.ts
+++ src/cmd_line/commands/read.ts
@@ -46,33 +46,40 @@ export class ReadCommand extends ExCommand {
   }
 
   async execute(vimState: VimState): Promise<void> {
-    const textToInsert = await this.getTextToInsert();
+    const textToInsert = await this.getTextToInsert(vimState);
     if (textToInsert) {
-      await TextEditor.insert(vimState.editor, textToInsert);
+      vimState.recordedState.transformer.insert(vimState.cursorStopPosition.getLineEnd(), '\n' + textToInsert);
     }
   }
 
-  async getTextToInsert(): Promise<string> {
-    if ('file' in this.arguments && this.arguments.file.length > 0) {
-      return readFileAsync(this.arguments.file, 'utf8');
-    } else if ('cmd' in this.arguments && this.arguments.cmd.length > 0) {
-      if (SUPPORT_READ_COMMAND) {
-        const cmd = this.arguments.cmd;
-        return new Promise<string>(async (resolve, reject) => {
-          const { exec } = await import('child_process');
-          exec(cmd, (err, stdout, stderr) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(stdout);
-            }
+  // TODO: executeWithRange()
+
+  async getTextToInsert(vimState: VimState): Promise<string> {
+    if ('file' in this.arguments) {
+        return readFileAsync(this.arguments.file, 'utf8');
+    } else if ('cmd' in this.arguments) {
+      if (this.arguments.cmd.length > 0) {
+        if (SUPPORT_READ_COMMAND) {
+          const cmd = this.arguments.cmd;
+          return new Promise<string>(async (resolve, reject) => {
+            const { exec } = await import('child_process');
+            exec(cmd, (err, stdout, stderr) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(stdout);
+              }
+            });
           });
-        });
+        } else {
+          return '';
+        }
       } else {
+        // TODO: error message?
         return '';
       }
     } else {
-      throw Error('Invalid arguments');
+      return vimState.document.getText();
     }
   }
 }
