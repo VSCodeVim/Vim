f02ed71ed848d8aca2e1fef77d8d2490a3b9e1e7
Jason Fields <jasonfields4@gmail.com>
2023-03-01 17:33:27 -0500

    Remove "Report bug" popup in favor of simply logging an error message
    This has helped resolve a few bugs, but has also generated more spam issues than I can keep up with. To be really useful it needs source maps, which are quite bulky.
    If we want to revive something like this, we should use VS Code's relatively new telemetry API.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ const config = {
     libraryTarget: 'commonjs2',
     devtoolModuleFilenameTemplate: '../[resource-path]',
   },
-  devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
   },
