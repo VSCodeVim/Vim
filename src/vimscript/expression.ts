1d09e2a8b7b8bc4bd44f286163a6a86b3a30cbeb
Jason Fields <jasonfields4@gmail.com>
2023-03-01 19:53:25 -0500

    Add `.prettierignore` and `yarn prettier` command

--- src/vimscript/expression.ts
+++ src/vimscript/expression.ts
@@ -1,8 +1,8 @@
-import { alt, any, Parser, regexp, string, noneOf } from "parsimmon";
-import { configuration } from "../configuration/configuration";
+import { alt, any, Parser, regexp, string, noneOf } from 'parsimmon';
+import { configuration } from '../configuration/configuration';
 
-const leaderParser = regexp(/<leader>/).map(() => configuration.leader);  // lazy evaluation of configuration.leader
-const specialCharacters = regexp(/<(?:Esc|C-\w|A-\w|C-A-\w)>/)
+const leaderParser = regexp(/<leader>/).map(() => configuration.leader); // lazy evaluation of configuration.leader
+const specialCharacters = regexp(/<(?:Esc|C-\w|A-\w|C-A-\w)>/);
 
 const specialCharacterParser = alt(specialCharacters, leaderParser);
 
@@ -17,10 +17,10 @@ const escapedParser = string('\\')
       return '\n';
     }
     return '\\' + escaped;
-  })
+  });
 
 export const keystrokesExpressionParser: Parser<string[]> = alt(
   escapedParser,
   specialCharacterParser,
-  noneOf('"'),
+  noneOf('"')
 ).many();
