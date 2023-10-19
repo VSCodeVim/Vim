2a72e5c38a0dacb5062f88545daccbea61bbd9af
Chandradeep Dey <github@chandradeepdey.com>
2023-10-19 10:09:09 +0530

    Add white as the default status bar foreground colour. (#8572)
    Fixes illegible text on the new 'Light Modern' theme.
    Fixes #8658.

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -306,12 +306,12 @@ class Configuration implements IConfiguration {
   statusBarColorControl = false;
 
   statusBarColors: IModeSpecificStrings<string | string[]> = {
-    normal: '#005f5f',
-    insert: '#5f0000',
-    visual: '#5f00af',
-    visualline: '#005f87',
-    visualblock: '#86592d',
-    replace: '#000000',
+    normal: ['#005f5f', '#ffffff'],
+    insert: ['#5f0000', '#ffffff'],
+    visual: ['#5f00af', '#ffffff'],
+    visualline: ['#005f87', '#ffffff'],
+    visualblock: ['#86592d', '#ffffff'],
+    replace: ['#000000', '#ffffff'],
   };
 
   searchHighlightColor = '';
