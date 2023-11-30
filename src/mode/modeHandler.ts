badd9bb153105c48fb1786120bb5bc10978c7bda
HenryTSZ <lh69jh@gmail.com>
2023-11-30 15:28:07 +0800

    Fix #8576 Cursor position is broken at v1.26.0+

--- src/mode/modeHandler.ts
+++ src/mode/modeHandler.ts
@@ -1282,6 +1282,11 @@ export class ModeHandler implements vscode.Disposable, IModeHandler {
             }
             break;
 
+          case Mode.Insert:
+            // Don't collapse existing selections in insert mode
+            selections.push(new vscode.Selection(start, stop));
+            break;
+
           default:
             // Note that this collapses the selection onto one position
             selections.push(new vscode.Selection(stop, stop));
