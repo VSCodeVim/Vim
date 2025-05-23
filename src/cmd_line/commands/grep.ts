7991c7d5fee10e8c80e0428677b925c65440512d
Parviz Azimov <azimovp17@gmail.com>
2025-05-23 06:32:23 +0300

    remove eslint-disable

--- src/cmd_line/commands/grep.ts
+++ src/cmd_line/commands/grep.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import * as vscode from 'vscode';
 
 import * as error from '../../error';
