d76f29230d8ea4e3cf572985498d0bac32b97545
Matthias Gessinger <matthias@gessinger.de>
2025-08-19 21:21:33 +0200

    Show action button in QuickPick when displaying registers

--- src/cmd_line/commands/register.ts
+++ src/cmd_line/commands/register.ts
@@ -2,46 +2,65 @@ import * as vscode from 'vscode';
 
 // eslint-disable-next-line id-denylist
 import { Parser, any, optWhitespace } from 'parsimmon';
-import { ErrorCode, VimError } from '../../error';
-import { Register } from '../../register/register';
+import { Register, RegisterContent } from '../../register/register';
 import { RecordedState } from '../../state/recordedState';
 import { VimState } from '../../state/vimState';
-import { StatusBar } from '../../statusBar';
 import { ExCommand } from '../../vimscript/exCommand';
+
+class RegisterDisplayItem implements vscode.QuickPickItem {
+  public readonly label: string;
+  public readonly description: string;
+
+  public readonly content: RegisterContent | undefined;
+  public readonly stringContent: string;
+
+  public readonly buttons: readonly vscode.QuickInputButton[];
+
+  constructor(registerKey: string, content: RegisterContent | undefined) {
+    this.label = registerKey;
+    this.content = content;
+    this.stringContent = '';
+    this.description = '';
+    this.buttons = [];
+
+    if (typeof content === 'string') {
+      this.stringContent = content;
+      this.description = this.stringContent;
+    } else if (content instanceof Array) {
+      this.stringContent = content.join('\n');
+      this.description = this.stringContent;
+    } else if (content instanceof RecordedState) {
+      this.description = content.actionsRun.map((x) => x.keysPressed.join('')).join('');
+    }
+
+    // maximum length of 100 characters for the description
+    this.description = this.description.slice(0, 100);
+
+    if (this.stringContent !== '') {
+      this.buttons = [
+        {
+          tooltip: 'Paste',
+          iconPath: new vscode.ThemeIcon('clippy'),
+        },
+      ];
+    }
+  }
+}
 
 export class RegisterCommand extends ExCommand {
   public override isRepeatableWithDot: boolean = false;
+  private readonly registerKeys: string[];
 
   public static readonly argParser: Parser<RegisterCommand> = optWhitespace.then(
     // eslint-disable-next-line id-denylist
     any.sepBy(optWhitespace).map((registers) => new RegisterCommand(registers)),
   );
 
-  private readonly registers: string[];
   constructor(registers: string[]) {
     super();
-    this.registers = registers;
-  }
-
-  private async getRegisterDisplayValue(register: string): Promise<string | undefined> {
-    let result = (await Register.get(register))?.text;
-    if (result instanceof Array) {
-      result = result.join('\n').substr(0, 100);
-    } else if (result instanceof RecordedState) {
-      result = result.actionsRun.map((x) => x.keysPressed.join('')).join('');
-    }
-
-    return result;
-  }
-
-  async displayRegisterValue(vimState: VimState, register: string): Promise<void> {
-    let result = await this.getRegisterDisplayValue(register);
-    if (result === undefined) {
-      StatusBar.displayError(vimState, VimError.fromCode(ErrorCode.NothingInRegister, register));
-    } else {
-      result = result.replace(/\n/g, '\\n');
-      void vscode.window.showInformationMessage(`${register} ${result}`);
-    }
+    this.registerKeys = Register.getKeys()
+      .filter((reg) => reg !== '_' && (registers.length === 0 || registers.includes(reg)))
+      .sort((reg1: string, reg2: string) => this.regSortOrder(reg1) - this.regSortOrder(reg2));
   }
 
   private regSortOrder(register: string): number {
@@ -59,33 +78,41 @@ export class RegisterCommand extends ExCommand {
     }
   }
 
-  async execute(vimState: VimState): Promise<void> {
-    if (this.registers.length === 1) {
-      await this.displayRegisterValue(vimState, this.registers[0]);
-    } else {
-      const currentRegisterKeys = Register.getKeys()
-        .filter(
-          (reg) => reg !== '_' && (this.registers.length === 0 || this.registers.includes(reg)),
-        )
-        .sort((reg1: string, reg2: string) => this.regSortOrder(reg1) - this.regSortOrder(reg2));
-      const registerKeyAndContent = new Array<vscode.QuickPickItem>();
+  async execute(_: VimState): Promise<void> {
+    const quickPick = vscode.window.createQuickPick<RegisterDisplayItem>();
 
-      for (const registerKey of currentRegisterKeys) {
-        const displayValue = await this.getRegisterDisplayValue(registerKey);
-        if (typeof displayValue === 'string') {
-          registerKeyAndContent.push({
-            label: registerKey,
-            description: displayValue,
+    quickPick.items = await Promise.all(
+      this.registerKeys.map(async (r) => {
+        const register = await Register.get(r);
+        return new RegisterDisplayItem(r, register?.text);
+      }),
+    );
+
+    quickPick.onDidChangeSelection((items) => {
+      if (items.length === 0) {
+        return;
+      }
+      const item = items[0];
+
+      vscode.window.showInformationMessage(`${item.label} ${item.stringContent}`);
+      quickPick.dispose();
+    });
+
+    quickPick.onDidTriggerItemButton((event) => {
+      const content = event.item.stringContent;
+
+      if (content !== '') {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+          editor.edit((editBuilder) => {
+            editBuilder.insert(editor.selection.end, content);
           });
         }
       }
 
-      void vscode.window.showQuickPick(registerKeyAndContent).then(async (val) => {
-        if (val) {
-          const result = val.description;
-          void vscode.window.showInformationMessage(`${val.label} ${result}`);
-        }
-      });
-    }
+      quickPick.dispose();
+    });
+
+    quickPick.show();
   }
 }
